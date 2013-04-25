fe387df7ce03e4a7b229032d440d25b92e889b65
jeff ilse <jli@ilseus.net>
2013-04-25 15:18:24 -0500

    has to stop leaving 'debugger' in the code.

--- js/controllers/loginCtrl.js
+++ js/controllers/loginCtrl.js
@@ -2,7 +2,6 @@
 
 $451.app.controller('LoginCtrl', function LoginCtrl($scope, $http, LoginService) {
     $scope.Login = function() {
-        debugger;
         LoginService.save($scope.user, function(response) {
             //console.dir(response);
         });
