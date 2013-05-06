d9d6fa32eeb2fbb8ffff56782546f2309b82bed6
Steve Davis <sdavis@four51.com>
2013-05-06 08:55:23 -0500

    ng-include all index.html inserted partials

--- app/js/controllers/navCtrl.js
+++ app/js/controllers/navCtrl.js
@@ -1,7 +1,6 @@
 'use strict';
 
-$451.app.controller('NavCtrl', function ($rootScope, $scope ) {
-    console.log("loading top nav ctrl");
+$451.app.controller('NavCtrl', function ($rootScope, $scope, $routeParams) {
 	$scope.appname = $451.app.name;
     $scope.Logout = function(){
         $451.clear();
