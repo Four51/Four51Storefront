adc11f78bdc4bf89709610cbff9b6ef497ed779e
rmelanson0451 <rmelanson@four51.com>
2013-05-15 14:57:08 -0500

    modified list of JS files karma monitors, attempted to write a e2e test

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -7,7 +7,8 @@ files = [
   'app/lib/angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
   'app/js/**/*.js',
-  'test/unit/**/*.js'
+  'test/unit/appSpec.js',
+  'test/unit/routingSpec.js'
 ];
 
 autoWatch = true;
