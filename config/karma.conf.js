7876eee7e5a99018019ebe3a3abebffd2c974955
rmelanson0451 <rmelanson@four51.com>
2013-05-17 13:41:48 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/karma.conf.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -4,15 +4,15 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
+  'app/lib/angular/angular.1.1.4.js',  //unsure if including both these is good or bad or what...
+  'app/lib/angular/angular-*.js',  //I don't really like implicitly loading these scripts if unnecessary.  Should we explicitly load them? -RM
   'test/lib/angular/angular-mocks.js',
   'app/lib/angular/ui-bootstrap-0.2.0.js',
   'app/js/app.js',
   'app/js/451.js',
   'app/js/**/*.js',
-  'test/unit/appSpec.js',
-  'test/unit/routingSpec.js',
-  'test/unit/451Spec.js'
+  'test/unit/appSpec.js',,
+  //'test/unit/451Spec.js'
 ];
 
 autoWatch = true;
