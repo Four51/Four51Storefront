f0efe8fca657c66d749b5d253c40c14909ddfb94
Steve Davis <sdavis@four51.com>
2013-05-29 14:52:16 -0500

    breaking our favorite orders into individual files

--- app/js/controllers/orderViewCtrl.js
+++ app/js/controllers/orderViewCtrl.js
@@ -27,19 +27,3 @@ four51.app.controller('OrderViewCtrl', function OrderViewCtrl($scope, $routePara
 	};
 });
 
-four51.app.controller('FavoriteOrderCtrl', function FavoriteOrderCtrl($scope, $routeParams, FavoriteOrderService) {
-	$scope.favoriteorders = FavoriteOrderService.query();
-	//$scope.order = OrderService.get({ id: $routeParams.id });
-	$scope.repeat = function(order) {
-		console.log('repeat order: ' + order.ID);
-	};
-	$scope.checkAll = function(event) {
-		angular.forEach($scope.favoriteorders, function(order) {
-			order.Selected = event.toElement.checked;
-		});
-	};
-	$scope.deleteSelected = function(event) {
-		event.preventDefault();
-		$scope.favoriteorders = FavoriteOrderService.delete($scope.favoriteorders);
-	};
-});
--- app/js/services/orderService.js
+++ app/js/services/orderService.js
@@ -16,30 +16,5 @@ four51.app.factory('OrderService', function($resource, $451, $api) {
 	}
 });
 
-four51.app.factory('FavoriteOrderService', function($resource, $451, $api) {
-	var resource = $resource($451.api('favoriteorder'), { }, {
-		'query': { method: 'GET', isArray: true },
-		'delete': { method: 'DELETE' }
-	});
-	return {
-		query: function() {
-			return $api.resource(resource).options({ persists: true, key: 'FavoriteOrders'}).query();
-		},
-		save: function(order) {
-			resource.save(order);
-			$451.clear('Order.' + order.ID);
-		},
-		delete: function(o) {
-			angular.forEach(o, function(order) {
-				if (order.Selected) {
-					resource.delete(order, function() {
-						$451.slice(o, order);
-						$451.clear('FavoriteOrders');
-					});
-				}
-			});
-			return o;
-		}
-	}
-});
+
 
