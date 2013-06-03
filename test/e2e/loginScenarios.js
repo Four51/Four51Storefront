0c8c294c12820774995d09cd6640f7edee771bbe
rmelanson0451 <rmelanson@four51.com>
2013-06-03 13:47:50 -0500

    made more efficient, added some scenarios, enhanced functionality

--- test/e2e/loginScenarios.js
+++ test/e2e/loginScenarios.js
@@ -2,29 +2,66 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 //login and logout test Scenarios
+var C_debug = false;
 
-describe('SPA login', function() {
+describe('SPA login with valid user/pass', function() {
+   it("should navigate to the index page to login", function() {
+        browser().navigateTo('../../app/index.html');
+    });
 
-  beforeEach(function() {
-    browser().navigateTo('../../app/index.html');
-  });
+    e2eLogin("sesendpo","fails555", C_debug);
 
-
-  xit('should automatically redirect to /catalog when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/catalog");
-  });
-
-
-  describe('login_form', function() {
-
-    e2eLogin("sesendpo","fails345", true);
-
-  });
-
-  describe('logout', function(){
-    e2eLogout(true);
-  });
+    e2eLogout(C_debug);
 
 });
 
-//more login/logout test scenarios
+describe('SPA login without valid user/pass', function() {
+    it("should navigate to the index page to login", function() {
+        browser().navigateTo('../../app/index.html');
+    });
+
+    e2eLogin("failme","fails345", C_debug);
+
+    it("should display a line saying cannot find user or password", function() {
+        expect(element('p:contains("not found")').count()).toBe(1); //this won't work if the client changes the messages or we localize it.
+        //this should be done by identifying a unique element ID... I think.
+     });
+
+});
+
+describe('SPA attempt to view a product without authentication', function() {
+    it("tries to go to the product page", function() {
+        browser().navigateTo('../../app/index.html#/product/A0151242-FB0C-4F0E-BB59-C7FA04167A74');
+    });
+
+    it('but gets a login box instead', function() {
+        expect(element('#login_box:visible').count()).toBe(1);
+        //we found a login box.  is login_box the best way to check for it's existence?
+    });
+
+});
+
+describe('SPA attempt to view a product WITH authentication', function() {
+    it("tries to go to the product page", function() {
+        browser().navigateTo('../../app/index.html#/product/A0151242-FB0C-4F0E-BB59-C7FA04167A74');
+    });
+
+    it('but gets a login box instead', function() {
+        expect(element('#login_box:visible').count()).toBe(1);
+        //we found a login box.  is login_box the best way to check for it's existence?
+    });
+
+    e2eLogin("sesendpo","fails555", C_debug);
+    //so we login, and should be shown the product we requested
+    it('should now show us the product page we requested and logged in for', function() {
+        expect(browser().location().url()).toBe("/product/A0151242-FB0C-4F0E-BB59-C7FA04167A74");
+    });
+
+    it('should hide the login box', function(){
+        //now let's check for that login box and make sure it DOESN'T show up
+        expect(element('#login_box:visible').count()).toBe(0);
+    });
+
+    //cleanup time, let's logout
+    e2eLogout(C_debug);
+});
