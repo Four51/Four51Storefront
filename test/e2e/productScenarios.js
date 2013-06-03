f7c8edd1be10ebe7368daf0305ac1f82980a22f5
rmelanson0451 <rmelanson@four51.com>
2013-06-03 14:04:55 -0500

    made more efficient, still needs product cases

--- test/e2e/productScenarios.js
+++ test/e2e/productScenarios.js
@@ -2,65 +2,64 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 //product test Scenarios
+var C_debug = false;
 
 describe('Product login', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('../../app/index.html');
+    it("should navigate to the index page to login", function() {
+        browser().navigateTo('../../app/index.html');
+    });
+
+    e2eLogin("sesendpo","fails555", C_debug);
+});
+
+describe('product list view and detail view', function() {
+
+
+  it('should automatically redirect to /catalog when the location hash/fragment is empty', function() {
+      expect(browser().location().url()).toBe("/catalog");
   });
 
-  describe('login_form', function() {
+  it('should render a navigation bar on the side', function() {
 
-    e2eLogin("sesendpo","fails345", false);
+      browser().navigateTo('#/catalog');
 
-  });
+     //check existence of categories
+      expect(repeater('.nav-header').count()).toBeGreaterThan(0);
+      //okay, there is at least one category, hooray
 
-  describe('product list view and detail view', function() {
-      it('should automatically redirect to /catalog when the location hash/fragment is empty', function() {
-          expect(browser().location().url()).toBe("/catalog");
-      });
+      element('.nav-list a:first').click(); //clicks first category nav link
+      element('.nav-header li a:first').click(); //clicks first category's subcategory nav link
+      element('.nav-header:nth-child(2) a:first').click(); //clicks second category nav link
+      element('.nav-header:nth-child(3) a:first').click(); //clicks third category nav link
+      if(C_debug){pause();}
 
-      it('should render a navigation bar on the side', function() {
+        //product testing
 
-          browser().navigateTo('#/catalog');
-          pause();
-          //check existence of categories
-          expect(repeater('.nav-header').count()).toBeGreaterThan(0);
-          //okay, there is at least one category, hooray
+        //let's use the category navigation code above to click a side nav link, then find a main-content-area subcategory link
+        //let's click that link, check that the interopID matches, and look at the product
 
-          pause();
-          element('.nav-list a:first').click(); //clicks first category nav link
-          element('.nav-header li a:first').click(); //clicks first category's subcategory nav link
-          element('.nav-header:nth-child(2) a:first').click(); //clicks second category nav link
-          element('.nav-header:nth-child(3) a:first').click(); //clicks third category nav link
-          pause();
+        //what are the observable attributes and fields of the product, on the product listing view?
 
-            //product testing
+        //once we verify the visible properties of the product on the product listing view, let's pick one and click it.
 
-            //let's use the category navigation code above to click a side nav link, then find a main-content-area subcategory link
-            //let's click that link, check that the interopID matches, and look at the product
+        //what are the observable attributes and fields of the product, on the product detail view?
 
-            //what are the observable attributes and fields of the product, on the product listing view?
+        //let's check the product object if we can, and verify that the fields match, one by one
 
-            //once we verify the visible properties of the product on the product listing view, let's pick one and click it.
+        //let's check different types of products...
+                  //product type 1
+                  //product type 2
+                  //etc...
 
-            //what are the observable attributes and fields of the product, on the product detail view?
-
-            //let's check the product object if we can, and verify that the fields match, one by one
-
-            //let's check different types of products...
-                      //product type 1
-                      //product type 2
-                      //etc...
-
-
-      });
 
   });
 
-  describe('logout', function(){
-    e2eLogout(false);
-  });
+});
+
+describe("logout/cleanup", function() {
+  //cleanup, let's log out.
+  e2eLogout(C_debug);
 
 });
 
