8f54a440086cfc143786cfbfa88f595d8fc994b0
rmelanson0451 <rmelanson@four51.com>
2013-06-21 10:36:55 -0500

    now testing for each route that exists so far

--- test/unit/routingSpec.js
+++ test/unit/routingSpec.js
@@ -1,22 +1,170 @@
 'use strict';
+/* these unit test specs test routing.js */
 
-/* jasmine specs for routing go here */
+var $scope, $route, $location, $httpBackend, C_debug;
+//beforeEach(module('451order')); //already did this in a previous file, we can only instantiate a module once
+C_debug = false;
 
-xdescribe('Routing: routing...', function(){
-   //test that correct template is loaded, and/or proper controller?
-   //test all URL scenarios
-        //at least one test for each page
-        //a test for each view?
-        //login/logout
-   //test malformed URLs or URLs with missing components
-   var router = module.$routeProvider;
-  xit("should determine correct path from URL",
-    inject(function(router) {
-        expect(router);
+function unitTestRoute(strHttpUrl, strHttpResponse,strLocationPath,strTemplateUrl,strControllerName){
+    inject(function(_$route_, _$location_, _$rootScope_, _$httpBackend_){
+        $route = _$route_;
+        $location = _$location_;
+        $scope = _$rootScope_;
+        $httpBackend = _$httpBackend_;
 
-    })
-  );
-    //htmlfilter = new 451orders.filter(html);
+        $httpBackend.when("GET",strHttpUrl).respond(strHttpResponse);
+
+        expect($route.current).toBeUndefined();
+        $location.path(strLocationPath);
+        $scope.$digest();
+
+        expect($route.current.templateUrl).toBe(strTemplateUrl);
+        expect($route.current.controller).toBe(strControllerName);
+
+        if(C_debug){
+            console.dir($location)
+            console.dir($route)
+        }
+    });
+}
 
 
+describe('$451 Routing:', function(){
+
+    //test malformed URLs or URLs with missing components?
+
+    it("should have a default route", function() {
+        inject(function(_$route_, _$location_, _$rootScope_, _$httpBackend_){
+            $route = _$route_;
+            $location = _$location_;
+            $scope = _$rootScope_;
+            $httpBackend = _$httpBackend_;
+
+            //here we go, this is the first mock in 451 history!  well, besides all the mocking that goes on within the dev team
+            //basically we are overriding the HTTPBackend service to not have a hissy fit when this virtual browser tries to grab that partial file
+            $httpBackend.when("GET",'partials/category.html').respond('category');
+
+            expect($route.current).toBeUndefined();
+
+            //$location.path('/foo'); //not setting a valid path helps make sure the routing behaves correctly
+            $scope.$digest();
+
+            expect($location.path()).toBe('/catalog');
+            expect($route.current).toBeDefined();
+
+            //by default we should end up at the category display
+            expect($route.current.templateUrl).toBe('partials/category.html');
+            expect($route.current.controller).toBe('CategoryCtrl');
+        });
+
+    });
+    it("should redirect to catelog if URL/path is invalid", function() {
+        inject(function(_$route_, _$location_, _$rootScope_, _$httpBackend_){
+            $route = _$route_;
+            $location = _$location_;
+            $scope = _$rootScope_;
+            $httpBackend = _$httpBackend_;
+
+            //because we expect to be redirected to the category route, set up this mock
+            $httpBackend.when("GET",'partials/category.html').respond('category');
+
+            expect($route.current).toBeUndefined();
+
+            $location.path('/foo');
+            $scope.$digest();
+            expect($location.path()).toBe('/catalog'); //should have redirected
+
+            expect($route.current).toBeDefined();
+
+            //by default or in the case of an unrecognized URL/path, we should always end up at the category display
+            expect($route.current.templateUrl).toBe('partials/category.html');
+            expect($route.current.controller).toBe('CategoryCtrl');
+        });
+
+    });
+    it("should route listorders requests properly", function() {  //this function should be duplicated for each route
+      //this could be replaced by a function call but I'll leave it around just for reference for now
+
+        inject(function(_$route_, _$location_, _$rootScope_, _$httpBackend_){
+          $route = _$route_;
+          $location = _$location_;
+          $scope = _$rootScope_;
+          $httpBackend = _$httpBackend_;
+
+          $httpBackend.when("GET",'partials/listOrders.html').respond('listOrders');
+
+          expect($route.current).toBeUndefined();
+          $location.path('/listOrders');
+          $scope.$digest();
+
+          expect($route.current.templateUrl).toBe('partials/listOrders.html');
+          expect($route.current.controller).toBe('ListOrdersCtrl');
+      });
+
+    });
+    it("should route orderdetails requests properly", function() {
+
+        unitTestRoute('partials/orderDetails.html','orderDetails','/orderdetails/','partials/orderDetails.html','OrderDetailsCtrl')
+
+    });
+    it("should route catalog requests properly", function() {
+
+        unitTestRoute('partials/category.html','catalog','/catalog','partials/category.html','CategoryCtrl')
+
+    });
+    it("should route catalog detail requests properly", function() {
+
+        unitTestRoute('partials/category.html','catalog','/catalog/:categoryInteropID','partials/category.html','CategoryCtrl')
+
+    });
+    it("should route product detail requests properly", function() {
+
+        unitTestRoute('partials/product.html','product','/product/:productInteropID','partials/product.html','ProductCtrl')
+
+    });
+    it("should route variant detail requests properly", function() {
+
+        unitTestRoute('partials/product.html','v','/product/:productInteropID/v/:variantInteropID','partials/product.html','ProductCtrl')
+
+    });
+    it("should route order requests properly", function() {
+
+        unitTestRoute('partials/orderSearch.html','order','/order','partials/orderSearch.html','OrderSearchCtrl')
+
+    });
+    it("should route order detail requests properly", function() {
+
+        unitTestRoute('partials/orderView.html','order','/order/:id','partials/orderView.html','OrderViewCtrl')
+
+    });
+    it("should route favorite order requests properly", function() {
+
+        unitTestRoute('partials/favoriteOrderList.html','favoriteorders','/favoriteorders','partials/favoriteOrderList.html','FavoriteOrderCtrl')
+
+    });
+    it("should route line item requests properly", function() {
+
+        unitTestRoute('partials/lineItemView.html','lineitem','/lineitem/:orderid/:lineitemid/','partials/lineItemView.html','LineItemViewCtrl')
+
+    });
+    it("should route message list requests properly", function() {
+
+        unitTestRoute('partials/messageList.html','message','/message','partials/messageList.html','MessageListCtrl')
+
+    });
+    it("should route message detail requests properly", function() {
+
+        unitTestRoute('partials/messageView.html','message','/message/:id','partials/messageView.html','MessageViewCtrl')
+
+    });
+    it("should route admin requests properly", function() {
+
+        unitTestRoute('partials/userView.html','admin','/admin','partials/userView.html','UserEditCtrl')
+
+    });
+    it("should route login requests properly", function() {
+
+        unitTestRoute('partials/login.html','login','/login','partials/login.html','LoginCtrl')
+
+    });
 });
