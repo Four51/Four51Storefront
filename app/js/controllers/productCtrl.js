4c32849b1c1e50335d78c62813dc28bfba04fc45
jeff ilse <jli@ilseus.net>
2013-06-26 14:44:02 -0500

    starting project to share product scope logic with product list.

--- app/js/controllers/productCtrl.js
+++ app/js/controllers/productCtrl.js
@@ -1,19 +1,10 @@
 four51.app.controller('ProductCtrl', function ($routeParams, $scope, ProductService, OrderService, $451) {
-
     $scope.product = ProductService.get({interopID: $routeParams.productInteropID}, function(data){
-        if($routeParams.variantInteropID){
-            var v = $451.filter(data.Variants, {Property: 'InteropID', Value: $routeParams.variantInteropID})[0];
-            $scope.variant = v;
-            $scope.priceSchedule = v.StandardPriceSchedule ? v.StandardPriceSchedule : data.StandardPriceSchedule; //include user permissions to decide to show
-            $scope.StaticSpecGroups = v.StaticSpecGroups || data.StaticSpecGroups;
-        }else{
-            $scope.priceSchedule = ProductService.HasVariantOverridePS(data, 'StandardPriceSchedule') ? null : data.StandardPriceSchedule; //don't show price schedule if variant overrides parent PS
-            $scope.StaticSpecGroups = data.StaticSpecGroups;
-        }
+        var v = null;
+        if($routeParams.variantInteropID)
+            v = $451.filter(data.Variants, {Property: 'InteropID', Value: $routeParams.variantInteropID})[0];
 
-        $scope.showInventory = (data.QuantityAvailable || ($scope.variant && $scope.variant.QuantityAvailable)) && data.DisplayInventory == true; //add some logic around user permissions
+        ProductService.modifyProductScope(data, v , $scope)
     });
     $scope.OrderService = OrderService;
-
-
 });
