55520e59fd0fd18b19374ec96e53f7008fecd580
jeff ilse <jli@ilseus.net>
2013-06-27 14:22:40 -0500

    price schedule tweaks

--- app/js/controllers/lineItemCtrl.js
+++ app/js/controllers/lineItemCtrl.js
@@ -3,7 +3,4 @@
 four51.app.controller('LineItemViewCtrl', function ($scope, $routeParams, OrderService) {
     $scope.order = OrderService.get({ id: $routeParams.orderid });
 	$scope.lineItemIndex = $routeParams.lineitemid;
-	$scope.hasPriceSchedule = function() {
-		return $scope.order.LineItems[$scope.lineItemIndex].PriceSchedule.PriceBreaks.length > 0;
-	}
 });
