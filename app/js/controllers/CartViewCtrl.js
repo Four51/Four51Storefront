328e5c1c3591af9c6a6063adfada6b8635ef5aeb
Steve Davis <sdavis@four51.com>
2013-07-05 14:54:27 -0500

    added an init to the cart view so we could be absolutely certain the current order id is in scope.

--- app/js/controllers/CartViewCtrl.js
+++ app/js/controllers/CartViewCtrl.js
@@ -1,15 +1,16 @@
 
 four51.app.controller('CartViewCtrl', function ($scope, $location, $451, OrderService, UserService) {
-    var currentOrderID = UserService.current.CurrentOrderID;
-    $scope.order = currentOrderID != null ? OrderService.get({ id: currentOrderID }) : null;
     $scope.checkout = function() {
         console.log('checkout');
     };
     $scope.cancelOrder = function() {
         OrderService.delete($scope.order, function() {
             UserService.current.CurrentOrderID = null;
-            currentOrderID = null;
             $location.path('#/catalog');
         });
     };
+    $scope.loadCart = function() {
+        var currentOrderID = UserService.current.CurrentOrderID;
+        $scope.order = currentOrderID != null ? OrderService.get({ id: currentOrderID }) : null;
+    }
 });
