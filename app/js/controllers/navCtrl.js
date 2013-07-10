6088bd37a15890b4d61bc442b863ed0f0d3413dd
Steve Davis <sdavis@four51.com>
2013-07-10 10:35:14 -0500

    added shortcut to refresh the user

--- app/js/controllers/navCtrl.js
+++ app/js/controllers/navCtrl.js
@@ -7,6 +7,9 @@ four51.app.controller('NavCtrl', function ($location, $scope, $451, UserService)
 	    $location.path("/login");
     };
 	$scope.template = { url: 'partials/nav.html'};
+    $scope.refresh = function() {
+        UserService.refresh();
+    }
 });
 
 four51.app.controller('SideNavCtrl', function ($rootScope, $scope, CategoryService) {
