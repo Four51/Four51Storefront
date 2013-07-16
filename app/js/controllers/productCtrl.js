8d37a2c1325c704bc21e8fc9511192d4e372fa48
jeff ilse <jli@ilseus.net>
2013-07-16 12:41:48 -0500

    adding variable specs to lineitem

--- app/js/controllers/productCtrl.js
+++ app/js/controllers/productCtrl.js
@@ -1,8 +1,8 @@
 four51.app.controller('shortProductViewCtrl', function ($routeParams, $scope, ProductService, OrderService, VariantService, $451) {
 	$scope.LineItem = {};
 	$scope.LineItem.Product = $scope.p;
-	ProductService.setProductScope($scope.LineItem.Product, null, $scope);
-	$scope.allowAddToOrderInProductList = $scope.allowAddToOrder && $scope.lineItemSpecs.length == 0 && $scope.LineItem.Product.Type != 'VariableText';
+	ProductService.setProductScope($scope);
+	$scope.allowAddToOrderInProductList = $scope.allowAddToOrder && $scope.LineItem.Specs.length == 0 && $scope.LineItem.Product.Type != 'VariableText';
 });
 
 four51.app.controller('ProductCtrl', function ($routeParams, $scope, ProductService, OrderService, VariantService, $451) {
@@ -10,10 +10,12 @@ four51.app.controller('ProductCtrl', function ($routeParams, $scope, ProductServ
 
 	$scope.LineItem.Product = ProductService.get({interopID: $routeParams.productInteropID}, function(data){
         var v = null;
-        if($routeParams.variantInteropID)
-            v = $451.filter(data.Variants, {Property: 'InteropID', Value: $routeParams.variantInteropID})[0];
+        if($routeParams.variantInteropID){
+			$scope.LineItem.Variant = $451.filter(data.Variants, {Property: 'InteropID', Value: $routeParams.variantInteropID})[0];
+		}
+		console.log('call scope');
+        ProductService.setProductScope($scope)
 
-        ProductService.setProductScope(data, v, $scope)
     });
 
 	$scope.addToOrder = function(quantity, productInteropID, variantInteropID){
@@ -32,7 +34,7 @@ four51.app.controller('ProductCtrl', function ($routeParams, $scope, ProductServ
 		{
 			var specOptionIDs = [];
 			var hasAllVarDefiningSpecs = true;
-			$451.filter($scope.LineItem.Product.Specs, {Property: 'DefinesVariant', Value:true}, function(item){
+			$451.filter($scope.LineItem.Specs, {Property: 'DefinesVariant', Value:true}, function(item){
 				if(!item.Value)
 				{
 					hasAllVarDefiningSpecs = false;
@@ -41,9 +43,10 @@ four51.app.controller('ProductCtrl', function ($routeParams, $scope, ProductServ
 				specOptionIDs.push(item.Value);
 			})
 			if(hasAllVarDefiningSpecs){
-				var v = VariantService.search($scope.LineItem.Product.InteropID, specOptionIDs, function(data){
+				VariantService.search($scope.LineItem.Product.InteropID, specOptionIDs, function(data){
 					if(!data.IsDefaultVariant)
-						ProductService.setProductScope($scope.LineItem.Product, data, $scope)
+						$scope.LineItem.Variant = data;
+						ProductService.setProductScope($scope)
 				});
 			}
 		}
