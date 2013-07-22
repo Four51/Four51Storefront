0c2f899d40404d7373be7a508bd243f6401d2182
jeff ilse <jli@ilseus.net>
2013-07-22 12:31:58 -0500

    taking out a lot of extra data from line item product/variant and replacing it with appropriate line item hist data.

--- app/js/routing.js
+++ app/js/routing.js
@@ -12,7 +12,7 @@ four51.app.config(['$routeProvider', function($routeProvider) {
             when('/order', { templateUrl: 'partials/orderSearch.html', controller: 'OrderSearchCtrl' }).
 			when('/order/:id', { templateUrl: 'partials/reporting/orderHistoryView.html', controller: 'OrderViewCtrl' }).
 			when('/favoriteorders', { templateUrl: 'partials/favoriteOrderList.html', controller: 'FavoriteOrderCtrl' }).
-			when('/lineitem/:orderid/:lineitemid/', { templateUrl: 'partials/reporting/lineItemHistoryView.html', controller: 'LineItemViewCtrl' }).
+			when('/order/:orderid/:lineitemindex/', { templateUrl: 'partials/reporting/lineItemHistoryView.html', controller: 'LineItemViewCtrl' }).
 			when('/message', { templateUrl: 'partials/messageList.html', controller: 'MessageListCtrl' }).
 			when('/message/:id', { templateUrl: 'partials/messageView.html', controller: 'MessageViewCtrl' }).
             when('/admin', { templateUrl: 'partials/userView.html', controller: 'UserEditCtrl' }).
