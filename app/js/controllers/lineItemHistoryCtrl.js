c864d2c4ca5515d89a6befc32bc6fd5f17d17155
jeff ilse <jli@ilseus.net>
2013-07-25 11:35:42 -0500

    spec.value chanaged to text value on selection spec. Also added selectedspecoptionid to api

--- app/js/controllers/lineItemHistoryCtrl.js
+++ app/js/controllers/lineItemHistoryCtrl.js
@@ -3,12 +3,13 @@
 four51.app.controller('LineItemViewCtrl', function ($scope, $routeParams, OrderService, ProductService) {
     $scope.order = OrderService.get({ id: $routeParams.orderid }, function(data){
 		$scope.LineItem = data.LineItems[$routeParams.lineitemindex]; // $scope.lineItemIndex = $routeParams.lineitemindex;
-
+		//$scope.StaticSpecGroups.VariableSpecs = $scope.LineItem.Specs;
 		//static spec groups are not captured on line item so they require another product.get if we want them shown here.
 		//not setting this product to scope to avoid confusion between live product data and line item history data.
 		ProductService.get({interopID: $scope.LineItem.Product.InteropID}, function(data){
-			$scope.StaticSpecGroups = data.StaticSpecGroups;
+			$scope.StaticSpecGroups = data.StaticSpecGroups || {};
+			$scope.StaticSpecGroups.VariableSpecs = {Name: 'VariableSpecs', Specs: $scope.LineItem.Specs};
+			console.dir($scope.StaticSpecGroups)
 		});
 	});
-
 });
