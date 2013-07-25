688543a33e37d4783febaf02cf9af29a056fa5d3
jeff ilse <jli@ilseus.net>
2013-07-25 12:59:28 -0500

    squeeze line item specs in with static spec groups on line item hist view.

--- app/js/controllers/lineItemHistoryCtrl.js
+++ app/js/controllers/lineItemHistoryCtrl.js
@@ -2,14 +2,18 @@
 
 four51.app.controller('LineItemViewCtrl', function ($scope, $routeParams, OrderService, ProductService) {
     $scope.order = OrderService.get({ id: $routeParams.orderid }, function(data){
-		$scope.LineItem = data.LineItems[$routeParams.lineitemindex]; // $scope.lineItemIndex = $routeParams.lineitemindex;
-		//$scope.StaticSpecGroups.VariableSpecs = $scope.LineItem.Specs;
+
+		$scope.LineItem = data.LineItems[$routeParams.lineitemindex];
+
 		//static spec groups are not captured on line item so they require another product.get if we want them shown here.
 		//not setting this product to scope to avoid confusion between live product data and line item history data.
 		ProductService.get({interopID: $scope.LineItem.Product.InteropID}, function(data){
 			$scope.StaticSpecGroups = data.StaticSpecGroups || {};
-			$scope.StaticSpecGroups.VariableSpecs = {Name: 'VariableSpecs', Specs: $scope.LineItem.Specs};
-			console.dir($scope.StaticSpecGroups)
+			//slight hack to get the line item specs to show with the product static spec groups.
+			angular.forEach($scope.LineItem.Specs, function(item){
+				item.VisibleToCustomer = true;
+			})
+			$scope.StaticSpecGroups.VariableSpecs = {Name: 'Variable Specs', Specs: $scope.LineItem.Specs};
 		});
 	});
 });
