79c4ec1dee2aaa2f7d7eb707533037d183a8aec6
ionlyseespots <kburas@four51.com>
2013-08-16 09:35:12 -0500

    I did not merge four51Ctrl correctly, copied original from git-  resolved. also added active path back in- but i think i can do this differently here shortly

--- app/js/controllers/Four51Ctrl.js
+++ app/js/controllers/Four51Ctrl.js
@@ -1,14 +1,27 @@
-four51.app.controller('Four51Ctrl', function ($scope, $route, $451, SecurityService, UserService, CategoryService, SpendingAccountService) {
+four51.app.controller('Four51Ctrl', function ($scope, $location, $route, $451, SecurityService, UserService, CategoryService, SpendingAccountService) {
     $scope.scroll = 0;
     $scope.appname = $451.appname;
-	$scope.Four51User = UserService;
+    $scope.Four51User = SecurityService;
 
-	$scope.$on('event:auth-loginRequired', function() {	});
+    // http://stackoverflow.com/questions/12592472/how-to-highlight-a-current-menu-item-in-angularjs
+    $scope.getClass = function(path) {
+        var cur_path = $location.path().substr(0, path.length);
+        if (cur_path == path) {
+            if($location.path().substr(0).length > 1 && path.length == 1 )
+                return "";
+            else
+                return "active";
+        } else {
+            return "";
+        }
+    };
 
-	$scope.$on("$routeChangeSuccess", function() {
-		$scope.tree = CategoryService.tree();
+    $scope.$on('event:auth-loginRequired', function() {	});
+
+    $scope.$on("$routeChangeSuccess", function() {
+        $scope.tree = CategoryService.tree();
         $scope.SpendingAccounts = SpendingAccountService.query();
         $scope.user = UserService.get();
-	});
+    });
 });
 
