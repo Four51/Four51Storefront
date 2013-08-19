c7490734863612bc10299b1631fbede26c1dd7ff
ionlyseespots <kburas@four51.com>
2013-08-19 09:02:30 -0500

    save

--- app/js/directives.js
+++ app/js/directives.js
@@ -59,7 +59,7 @@ four51.app.directive('panel', function () {
         restrict:'E',
         transclude:true,
         scope:{ title:'@title' },
-        template:'<li class="nav-header animated fadeIn">' +
+        template:'<li class="nav-header visible-phone visible-tablet animated fadeIn">' +
             '<a ng-href=#/catalog ng-transclude></a>' +
             '</li>',
         replace:true
