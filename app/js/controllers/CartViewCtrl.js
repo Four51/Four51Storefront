18d0b3143cd6398a3f0fa103859731fb873c1424
Steve Davis <sdavis@four51.com>
2013-09-13 08:40:42 -0500

    removed lineitemgrid directive fixed binding for ship account

--- app/js/controllers/CartViewCtrl.js
+++ app/js/controllers/CartViewCtrl.js
@@ -40,4 +40,30 @@ four51.app.controller('CartViewCtrl', function ($scope, $location, $451, Order, 
 		$scope.currentOrder.Subtotal = newTotal;
 	}, true);
 
+    $scope.copyAddressToAll = function() {
+        angular.forEach($scope.currentOrder.LineItems, function(n) {
+            n.DateNeeded = $scope.currentOrder.LineItems[0].DateNeeded;
+        });
+    };
+
+    $scope.removeSelected = function() {
+        Order.save($scope.currentOrder, function(o) {
+            $scope.currentOrder = o;
+        });
+    };
+
+    // ng grid implementation
+    $scope.gridOptions = {
+        data: 'currentOrder.LineItems',
+        columnDefs: [
+            { displayName: 'Remove', field: 'Selected', cellTemplate: 'partials\\controls\\ngGridCheckBox.html' },
+            { displayName: 'ID', cellTemplate: "<a href=\"#/cart/{{row.getProperty('Product.ViewName')}}/{{row.rowIndex}}\">{{row.getProperty('ProductIDText')}}</a>"},
+            { displayName: 'Product', field: 'Product.Name'},
+            { displayName: 'Specifications', field: 'Specs', cellTemplate: 'partials\\controls\\ngGridList.html'},
+            { displayName: 'Unit Price', field: 'UnitPrice', cellFilter: 'currency' },
+            { displayName: 'Quantity', cellTemplate: "<div class='ngCellText colt{{$index}}'><quantityfield error='qtyError' lineitem='row.entity' />{{qtyError}}</div>"},
+            { displayName: 'Total Quantity', cellTemplate: '<div class="ngCellText colt{{$index}}">{{row.getProperty("Product.QuantityMultiplier") * row.getProperty("Quantity")}}</div>' },
+            { displayName: 'Price', field: 'LineTotal', cellFilter: 'currency' }
+        ]
+    }
 });
--- app/js/services/categoryService.js
+++ app/js/services/categoryService.js
@@ -1,5 +1,5 @@
 'use strict';
-four51.app.factory('Category', function($resource, $451){
+four51.app.factory('Category', function($resource, $cacheFactory, $451){
     var _get = function(interopID,success) {
         $resource($451.api('categories/:interopID', {interopID: '@ID'})).get({ 'interopID': interopID}).$promise.then(function(cat) {
             if (angular.isFunction(success))
@@ -8,7 +8,8 @@ four51.app.factory('Category', function($resource, $451){
     }
 
     var _query = function(success){
-        $resource($451.api('categories')).query().$promise.then(function(data){
+        var c = $cacheFactory;
+        $resource($451.api('categories'), {}, { query: { method: 'GET', isArray: true, cache: true }}).query().$promise.then(function(data){
             if (angular.isFunction(success))
                 success(data);
         });
