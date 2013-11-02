f82974bc764f619897d47c3536f4899f95f46b14
ionlyseespots <kburas@four51.com>
2013-11-01 20:21:51 -0500

    default product about updated for bootstrap 3

--- app/js/controllers/productCtrl.js
+++ app/js/controllers/productCtrl.js
@@ -37,7 +37,7 @@ four51.app.controller('shortProductViewCtrl', function ($routeParams, $scope, Pr
 
 four51.app.controller('ProductCtrl', function ($routeParams, $scope, Product, ProductDisplayService, Order, Variant, $451, $location, User) {
 	$scope.LineItem = {};
-	$scope.addToOrderText = "add to cart";
+	$scope.addToOrderText = "Add To Cart";
 	function ProductVariantGetsDone (){
 		ProductDisplayService.setNewLineItemScope($scope);
 		ProductDisplayService.setProductViewScope($scope);
@@ -132,3 +132,8 @@ four51.app.controller('CustomProductCtrlMatrix', function($scope, $451, Variant,
 		});
 	});
 });
+$scope.isActive = false;
+
+$scope.toggleActive = function () {
+    $scope.isActive = !$scope.isActive;
+};
