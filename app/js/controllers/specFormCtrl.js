c5cf957a8bd042f91026bd94206937a6c7c8d7dc
Jeff Ilse <jli@ilseus.net>
2013-11-19 16:19:55 -0600

    variable spec form

--- app/js/controllers/specFormCtrl.js
+++ app/js/controllers/specFormCtrl.js
@@ -1,6 +1,9 @@
 'use strict';
 
-four51.app.controller('SpecFormCtrl', function ($scope) {
-	$scope.test = "hello";
+four51.app.controller('SpecFormCtrl', function ($routeParams, $scope, ProductDisplayService) {
+	ProductDisplayService.getProductAndVariant($routeParams.productInteropID, $routeParams.variantInteropID, function(data){
+		$scope.Product = data.product;
+		$scope.Variant = data.variant;
+	})
 
 });
