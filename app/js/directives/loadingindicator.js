b08d8eab026b99fcdf48ee7748b73192964940a0
Steve Davis <sdavis@four51.com>
2013-11-27 16:36:25 -0600

    cleanup bad attempt at loading indicator

--- app/js/directives/loadingindicator.js
+++ app/js/directives/loadingindicator.js
@@ -1,9 +1,7 @@
 four51.app.directive('loadingindicator', function () {
     return {
-        restrict:'A',
-        scope: {
-	        title: '@title'
-        },
+        restrict:'E',
+        scope: { title: '@title' },
         template:'<div class="container col-xs-12 app-loading-indicator-cell"><span class="app-loading-indicator"></span><p class="app-loading-indicator-title">{{title}}</p></div>'
     };
 });
