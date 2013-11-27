1f0d6b3794b0a6609503bc9ab94d9e3099ecdb01
Steve Davis <sdavis@four51.com>
2013-11-27 16:15:44 -0600

    working on loader

--- app/js/directives/loadingindicator.js
+++ app/js/directives/loadingindicator.js
@@ -1,7 +1,9 @@
 four51.app.directive('loadingindicator', function () {
     return {
-        restrict:'E',
-        scope: { title: '@title' },
+        restrict:'A',
+        scope: {
+	        title: '@title'
+        },
         template:'<div class="container col-xs-12 app-loading-indicator-cell"><span class="app-loading-indicator"></span><p class="app-loading-indicator-title">{{title}}</p></div>'
     };
 });
