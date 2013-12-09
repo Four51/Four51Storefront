d18a229c0e9bf67f5847fa354ae8f89748bf992c
Jeff Ilse <jli@ilseus.net>
2013-12-09 13:12:31 -0600

    more tweaks for required variable specs

--- app/js/directives/customfields.js
+++ app/js/directives/customfields.js
@@ -23,7 +23,7 @@ four51.app.directive('customselectionfield', function($451) {
         template: '<label>{{customfield.Label}}</label>' +
             '<select ng-init="init()" ng-required="customfield.Required" ng-change="changed()" ng-model="item" ng-options="option.Value for option in customfield.Options" ng-if="customfield.Options">' +
             '<option ng-if="!customfield.IsRequired" value="" /></select>' +
-            '<input type="text" ng-change="otherChanged()" ng-model="other" ng-show="customfield.isOtherSelected" />',
+            '<input type="text" ng-change="otherChanged()" ng-model="other" ng-show="customfield.isOtherSelected" ng-required="customfield.Required && customfield.isOtherSelected" />',
         link: function(scope, element, attr) {
             scope.changed = function() {
 	            //reset values
