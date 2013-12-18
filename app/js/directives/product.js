7d992493978341c7362bff31587b5a0a00d3e264
Jeff Ilse <jli@ilseus.net>
2013-12-18 16:02:51 -0600

    SPA-10635  Checkout - inventory validation confused by Save Change

--- app/js/directives/product.js
+++ app/js/directives/product.js
@@ -170,8 +170,8 @@ four51.app.directive('quantityfield', function($451, ProductDisplayService){
 					//console.log('variant not selected can\'t check qty available'); //in vboss the user may select the qty before the variant. we may have to change when this gets called so inventory available can be re validated if the variant is chnaged based on a selection spec. It's probably not a big deal since the api will check inventory available on adding to order.
 				}
 				else{
+					var qtyAvail = (product.IsVariantLevelInventory ? variant.QuantityAvailable : product.QuantityAvailable) + (lineItem.OriginalQuantity || 0);
 
-					var qtyAvail = product.IsVariantLevelInventory ? variant.QuantityAvailable : product.QuantityAvailable + (lineItem.OriginalQuantity || 0);
 					if(qtyAvail < value && product.AllowExceedInventory == false){
 						scope.lineitem.qtyError = "not enough available inventory " +  qtyAvail;
 						scope.valid = false;
