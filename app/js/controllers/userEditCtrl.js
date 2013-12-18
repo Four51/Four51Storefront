15653bab1fb361fec77109266a43a176516ea2f0
Jeff Ilse <jli@ilseus.net>
2013-12-18 15:01:18 -0600

    SPA-10521  Remove Auto (temp user) InteropIDs

--- app/js/controllers/userEditCtrl.js
+++ app/js/controllers/userEditCtrl.js
@@ -1,19 +1,26 @@
 'use strict';
 
-four51.app.controller('UserEditCtrl', function ($scope, User) {
+four51.app.controller('UserEditCtrl', function ($scope, $location, User) {
 	$scope.loginasuser = {};
+
+	if($scope.user.Type != 'TempCustomer')
+		$scope.user.TempUsername = $scope.user.Username
+
 	$scope.save = function() {
+		$scope.user.Username = $scope.user.TempUsername;
 		$scope.displayLoadingIndicator = true;
         if($scope.user.Type == 'TempCustomer')
 			$scope.user.ConvertFromTempUser = true;
 
-		User.save($scope.user, function() {
+		User.save($scope.user, function(u) {
 			$scope.displayLoadingIndicator = false;
 			$scope.saveSuccess = true;
+			$scope.user.TempUsername = u.Username;
 		});
-
     };
 	$scope.loginExisting = function(){
-		User.login({Username: $scope.loginasuser.Username, Password:  $scope.loginasuser.Password, ID: $scope.user.ID, Type: $scope.user.Type});
+		User.login({Username: $scope.loginasuser.Username, Password:  $scope.loginasuser.Password, ID: $scope.user.ID, Type: $scope.user.Type},function(u){
+			$location.path("/catalog");
+		});
 	};
 });
