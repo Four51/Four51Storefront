90cc75f5f622a52433c7069d43785de69b118817
Steve Davis <sdavis@four51.com>
2014-01-07 13:11:49 -0600

    SPA-10673  Purchase order as only payment method invalidates billing

--- app/js/directives/checkout.js
+++ app/js/directives/checkout.js
@@ -7,7 +7,26 @@ four51.app.directive('paymentselector', function() {
                $scope.currentOrder.PaymentMethod = type;
                $rootScope.$broadcast('event:paymentMethodChange', type);
            };
+
+	       $scope.setBudgetAccount = function(count) {
+		       $scope.setPaymentMethod('BudgetAccount');
+		       if ($scope.currentOrder.BudgetAccountID || count > 1) return;
+		       angular.forEach($scope.SpendingAccounts, function(a) {
+			       if (a.AccountType.PurchaseCredit) {
+				       $scope.currentOrder.BudgetAccountID = a.ID;
+				       $scope.selectedBudgetAccount = a;
+			       }
+		       });
+	       };
+
+	      if ($scope.currentOrder.PaymentMethod == 'BudgetAccount' && $scope.currentOrder.BudgetAccountID)
+		       angular.forEach($scope.SpendingAccounts, function(a) {
+			       if (a.AccountType.PurchaseCredit && a.ID == $scope.currentOrder.BudgetAccountID) {
+				       $scope.selectedBudgetAccount = a;
+			       }
+		       });
+	       }
        }
-   }
+
    return obj;
 });
