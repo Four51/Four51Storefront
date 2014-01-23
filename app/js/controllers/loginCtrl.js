8178901e87acc3fc9432905c4703356303ff9f10
Steve Davis <sdavis@four51.com>
2014-01-23 09:10:07 -0600

    SPA-10760  Login - handle user password change requirements

--- app/js/controllers/loginCtrl.js
+++ app/js/controllers/loginCtrl.js
@@ -1,12 +1,27 @@
 'use strict';
 
-four51.app.controller('LoginCtrl', function LoginCtrl($scope, $route, User) {
+four51.app.controller('LoginCtrl', function LoginCtrl($scope, $sce, $route, User) {
 
 	$scope.$on('event:auth-loginFailed', function(event, message) {
-		$scope.errorMessage = message;
+		$scope.actionMessage = message;
 	});
 
+	var codes = ['PasswordSecurityException'];
+
 	$scope.login = function() {
-		User.login(this.credentials);
+		$scope.actionMessage = null;
+		// need to reset any error codes that might be set so we can handle new one's
+		angular.forEach(codes, function(c) {
+			$scope[c] = null;
+		});
+		User.login($scope.credentials, null, function(ex) {
+			$scope[ex.Code.text] = true;
+			if (ex.Code.is('PasswordSecurity'))
+				$scope.actionMessage = $sce.trustAsHtml(ex.Message);
+			$scope.credentials.Password = null;
+			$scope.credentials.CurrentPassword = null;
+			$scope.credentials.NewPassword = null;
+			$scope.credentials.ConfirmPassword = null;
+		});
 	};
 });
