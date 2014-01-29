37869d6b4bff0745d81e72f6388e7f0d0469b979
ionlyseespots <kburas@four51.com>
2014-01-29 14:09:00 -0600

    add back step directive to go back while browsing in top nav

--- app/js/directives/nav.js
+++ app/js/directives/nav.js
@@ -16,6 +16,18 @@ four51.app.directive('accountnavigation', function() {
     return obj;
 });
 
+//[back] button to help continue shopping without needing to force user to use [products] button http://stackoverflow.com/questions/14070285/how-to-implement-history-back-in-angular-js
+four51.app.directive('backStep', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('click', function () {
+                history.back();
+                scope.$apply();
+                })
+        }
+    }
+});
 // equal height directive, maybe we want bootstrap wells to all be same height- this works
 //four51.app.directive('sizeColumn', function() {
 //    return function(scope, element) {
