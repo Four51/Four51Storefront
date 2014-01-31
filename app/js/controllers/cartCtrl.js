8cff89d422f93373c73b2e18eff11e72e56145e3
ionlyseespots <kburas@four51.com>
2014-01-31 09:53:46 -0600

    response message and error message list with design and comments for ideation I discussed with Steve to implement

--- app/js/controllers/cartCtrl.js
+++ app/js/controllers/cartCtrl.js
@@ -1,4 +1,4 @@
-four51.app.controller('CartViewCtrl', function ($scope, $location, $451, Order, OrderConfig, User, GiftCard) {
+four51.app.controller('CartViewCtrl', function ($scope, $location, $451, Order, OrderConfig, User) {
     $scope.continueShopping = function() {
 	    if (!$scope.cart.$invalid) {
 	        if (confirm('Do you want to save changes to your order before continuing?') == true)
@@ -101,7 +101,7 @@ four51.app.controller('CartViewCtrl', function ($scope, $location, $451, Order, 
     };
 
     // $scope.$on('$viewContentLoaded', function () {
-    //    $('.view-footer').removeClass('ng-hide');
+    //    $('.view-footer').removeClass('navbar-fixed-bottom');
     // });
 
    // $scope.$on('$viewContentLoaded', function () {
