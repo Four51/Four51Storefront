79b69ea7ba31151c12c1101008b28bfce9a18a13
ionlyseespots <kburas@four51.com>
2014-03-18 13:18:52 -0500

    missed a file, ui-date.js needs to also be removed

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.date', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap']);
 
