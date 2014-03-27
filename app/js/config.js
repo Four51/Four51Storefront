2a520530daa7133a1ba381189de5851d59d73726
Robert <rwatt@four51.com>
2014-03-27 14:22:06 -0500

    added config.js back in configured the ui.bootstrap datepicker to remove button bars, weeks column, and append it to the body

---
+++ app/js/config.js
@@ -0,0 +1,18 @@
+four51.app.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(false);
+}]);
+
+four51.app.config(['$provide', function($provide) {
+    $provide.decorator('$exceptionHandler', function($delegate, $injector) {
+        return function $broadcastingExceptionHandler(ex, cause) {
+            $delegate(ex, cause);
+            $injector.get('$rootScope').$broadcast('exception', ex, cause);
+        }
+    });
+}]);
+
+four51.app.config(function(datepickerConfig, datepickerPopupConfig){
+    datepickerConfig.showWeeks = false;
+    datepickerPopupConfig.showButtonBar = false;
+    datepickerPopupConfig.appendToBody = true;
+});
