b474d8c498dc496339215e4c073a6034fb23448f
Steve Davis <sdavis@four51.com>
2014-04-08 09:48:01 -0500

    site text replacements

--- app/js/filters.js
+++ app/js/filters.js
@@ -32,6 +32,34 @@ four51.app.filter('r', ['$sce', 'WhiteLabel', function($sce, WhiteLabel) {
 	}
 }]);
 
+four51.app.filter('rc', ['$sce', 'WhiteLabel', function($sce, WhiteLabel) {
+	return function(value) {
+		var result = value, found = false;
+		angular.forEach(WhiteLabel.replacements, function(c) {
+			if (found) return;
+			if (c.key.toLowerCase() == value.toLowerCase()) {
+				result = $sce.trustAsHtml(c.value);
+				found = true;
+			}
+		});
+		return result;
+	}
+}]);
+
+four51.app.filter('rl', ['$sce', 'WhiteLabel', function($sce, WhiteLabel) {
+	return function(value) {
+		var result = value, found = false;
+		angular.forEach(WhiteLabel.replacements, function(c) {
+			if (found) return;
+			if (c.key.toLowerCase() == value.toLowerCase()) {
+				result = $sce.trustAsHtml(c.value.toLowerCase());
+				found = true;
+			}
+		});
+		return result;
+	}
+}]);
+
 four51.app.filter('noliverates', function() {
 	return function(value) {
 		var output = [];
