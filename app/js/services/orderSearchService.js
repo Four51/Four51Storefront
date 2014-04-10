b4ea2736ac0cea492427e358589ecdc0a5cc2e40
Steve Davis <sdavis@four51.com>
2014-04-10 09:55:54 -0500

    SPA-11242  Order Search - date filter criteria only return one result?

--- app/js/services/orderSearchService.js
+++ app/js/services/orderSearchService.js
@@ -1,9 +1,9 @@
 four51.app.factory('OrderSearch', ['$resource', '$451', function($resource, $451) {
 
     var _search = function(stat, success) {
-        $resource($451.api('order'),{},
-            { 'get': { method: 'GET', isArray: true }}
-        ).get(stat).$promise.then(function(list) {
+	    stat.DateRangeFrom = stat.DateRangeFrom.toISOString();
+	    stat.DateRangeTo = stat.DateRangeTo.toISOString();
+        $resource($451.api('order'),{}, { 'get': { method: 'GET', isArray: true }}).get(stat).$promise.then(function(list) {
             if (angular.isFunction(success))
                 success(list);
         });
