6f55f9767dbeceface474730c7e4675c8af76598
Steve Davis <sdavis@four51.com>
2014-04-16 12:39:11 -0500

    SPA-11362  Caching saved CC's - bad idea?

--- app/js/services/savedCreditCardService.js
+++ app/js/services/savedCreditCardService.js
@@ -6,17 +6,13 @@ four51.app.factory('SavedCreditCard', ['$resource', '$rootScope', '$451', functi
     }
 
     var _query = function(success) {
-	    var cards = store.get('451Cache.SavedCards');
-	    cards ? _then(success, cards) :
-			$resource($451.api('savedcreditcard')).query().$promise.then(function(list) {
-				store.set('451Cache.SavedCards', list);
-		        _then(success, list);
-			});
+		$resource($451.api('savedcreditcard')).query().$promise.then(function(list) {
+	        _then(success, list);
+		});
     };
 
 	var _delete = function(card, success) {
 		$resource($451.api('savedcreditcard')).delete(card, function() {
-			store.remove('451Cache.SavedCards');
 			_then(success);
 		});
 	};
