5df45cf19756379663eaac4362856fc37c145596
Steve Davis <sdavis@four51.com>
2014-04-24 15:35:21 -0500

    last bit of reporting

--- app/js/controllers/reportCtrl.js
+++ app/js/controllers/reportCtrl.js
@@ -1,10 +1,17 @@
 four51.app.controller('ReportCtrl', ['$scope', '$routeParams', '$451', 'Report',
 function($scope, $routeParams, $451, Report) {
 	$scope.displayLoadingIndicator = true;
-	Report.get($routeParams.id, function(data) {
-		$scope.report = data;
-		$scope.displayLoadingIndicator = false;
-	});
+	$scope.actionMessage = null;
+	Report.get($routeParams.id,
+		function(data) {
+			$scope.report = data;
+			$scope.displayLoadingIndicator = false;
+		},
+		function(ex) {
+			$scope.actionMessage = ex.Message;
+			$scope.displayLoadingIndicator = false;
+		}
+	);
 
 	$scope.saveReport = function() {
 		$scope.displayLoadingIndicator = true;
@@ -12,6 +19,7 @@ function($scope, $routeParams, $451, Report) {
 		Report.save($scope.report,
 			function(report) {
 				$scope.report = report;
+				$scope.actionMessage = "Your changes have been saved.";
 				$scope.displayLoadingIndicator = false;
 			},
 			function(ex) {
