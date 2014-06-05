501b61046f576b6b3dabb3aa6e817ba88e88952d
Robert <rwatt@four51.com>
2014-06-05 15:21:46 -0500

    Quick address feature

--- app/js/controllers/addressInputCtrl.js
+++ app/js/controllers/addressInputCtrl.js
@@ -37,4 +37,49 @@ function ($scope, $rootScope, $location, User, Address, Resources) {
         return ($scope.user.Permissions.contains('BillingAddressPhoneRequired') && $scope.address.IsBilling) ||
             ($scope.user.Permissions.contains('ShipAddressPhoneRequired') && $scope.address.IsShipping);
     }
+
+    var streetComplete = null;
+    var streetNum = null;
+    var streetName = null;
+    var tempShipping = null;
+    var tempBilling = null;
+
+    $scope.$watch('addressobj', function(newval, oldval) {
+        if (newval != oldval){
+            $scope.address.IsShipping   ? (tempShipping = true) : '';
+            $scope.address.IsBilling    ? (tempBilling = true)  : '';
+            $scope.address = {'Country':"US"};
+            tempShipping    ? $scope.address.IsShipping = tempShipping  : '';
+            tempBilling     ? $scope.address.IsBilling  = tempBilling   : '';
+            streetComplete  = null;
+            streetNum       = null;
+            streetName      = null;
+            $scope.makeAddress(newval);
+        }
+    }, true);
+
+    $scope.makeAddress = function(addressobj){
+        $scope.address.AddressName = addressobj.name;
+        addressobj.formatted_phone_number ? $scope.address.Phone = addressobj.formatted_phone_number : '';
+            angular.forEach(addressobj.address_components, function(component){
+            component.types[0] == 'street_number'   ? (streetNum = component.long_name)     : null;
+            component.types[0] == 'route'           ? (streetName = component.long_name)    : null;
+            if (streetNum || streetName){
+                if (streetNum && streetName){
+                    streetComplete = streetNum + ' ' + streetName;
+                }
+                else if (streetNum){
+                    streetComplete = streetNum;
+                }
+                else{
+                    streetComplete = streetName;
+                }
+                $scope.address.Street1 = streetComplete;
+            }
+            component.types[0] == 'locality'                    ? ($scope.address.City = component.long_name)       : '';
+            component.types[0] == 'administrative_area_level_1' ? ($scope.address.State = component.short_name)     : '';
+            component.types[0] == 'country'                     ? ($scope.address.Country = component.short_name)   : '';
+            component.types[0] == 'postal_code'                 ? ($scope.address.Zip = component.short_name)       : '';
+        });
+    }
 }]);
