13025025ccb10a3d0d41583d2f857c2e6efdcd45
Steve Davis <sdavis@four51.com>
2014-06-05 17:32:04 -0500

    minification bug on file not with right pattern

--- app/js/directives/addToOrderSpecs.js
+++ app/js/directives/addToOrderSpecs.js
@@ -1,7 +1,7 @@
-four51.app.directive('addtoorderspecs', function($routeParams) {
+four51.app.directive('addtoorderspecs', ['$routeParams',  function($routeParams) {
 	var obj = {
 		restrict: 'E',
 		templateUrl: 'addToOrderSpecForm.hcf?id=' + $routeParams.productInteropID
 	}
 	return obj;
-});
+}]);
