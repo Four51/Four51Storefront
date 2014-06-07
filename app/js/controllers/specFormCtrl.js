c5cf931205dde5cf07a1a2da6685535091156907
Jeff Ilse <jli@ilseus.net>
2014-06-07 10:35:51 -0500

    spec form doesn't validate required fields. Should be backward compatible with previously forked specforms

--- app/js/controllers/specFormCtrl.js
+++ app/js/controllers/specFormCtrl.js
@@ -1,5 +1,6 @@
-four51.app.controller('SpecFormCtrl', ['$scope', '$location', '$route', '$routeParams', 'ProductDisplayService', 'Variant',
-function ($scope, $location, $route, $routeParams, ProductDisplayService, Variant) {
+four51.app.controller('SpecFormCtrl', ['$scope', '$location', '$route', '$routeParams', '$window', 'ProductDisplayService', 'Variant',
+function ($scope, $location, $route, $routeParams, $window, ProductDisplayService, Variant) {
+	$scope.variantErrors = [];
 	var varID = $routeParams.variantInteropID == 'new' ? null :  $routeParams.variantInteropID;
 	$scope.loadingImage = true;
 	ProductDisplayService.getProductAndVariant($routeParams.productInteropID, varID, function(data){
@@ -18,19 +19,36 @@ function ($scope, $location, $route, $routeParams, ProductDisplayService, Varian
 			});
 		}
 	});
-
-	function saveVariant(variant) {
+	function validateVariant(){
+		if(!$scope.Variant) return;
+		var newErrors = [];
+		angular.forEach($scope.Variant.Specs, function(s){
+			if(s.Required && !s.Value)
+				newErrors.push(s.Label || s.Name + ' is a required field');
+		});
+		$scope.variantErrors = newErrors;
+	}
+	$scope.$watch('Variant.Specs', function(o, n){
+		validateVariant();
+	}, true);
+	function saveVariant(variant, saveNew, hideErrorAlert /*for compatibility*/) {
+		if($scope.variantErrors.length){
+			$scope.showVariantErrors = true;
+			if(!hideErrorAlert)
+				$window.alert("please fill in all required fields"); //the default spec form should be made to deal with showing $scope.variantErrors, but it's likely existing spec forms may not deal with $scope.variantErrors
+			return;
+		}
+		if(saveNew) $scope.Variant.InteropID = null;
 		Variant.save(variant, function(data){
 			$location.path('/product/' + $scope.Product.InteropID + '/'+ data.InteropID);
 		});
 	}
-	$scope.save = function(){
-		saveVariant($scope.Variant);
+	$scope.save = function(hideErrorWindowAlert){
+		saveVariant($scope.Variant, false, hideErrorWindowAlert);
 	}
 
-	$scope.saveasnew = function() {
-		$scope.Variant.InteropID = null;
-		saveVariant($scope.Variant);
+	$scope.saveasnew = function(hideErrorAlert) {
+		saveVariant($scope.Variant, true, hideErrorAlert);
 	}
 
 	$scope.$on('event:imageLoaded', function(event, result) {
