e9e6f01ed76054f10ab0e990cc91384137e966e1
Steve Davis <sdavis@four51.com>
2014-07-21 17:17:22 -0500

    SPA-10504  Support Multiple Shopping Carts

--- app/js/services/orderService.js
+++ app/js/services/orderService.js
@@ -1,9 +1,10 @@
 four51.app.factory('Order', ['$resource', '$rootScope', '$451', 'Security', 'Error', function($resource, $rootScope, $451, Security, Error) {
 	var _multipleShip = false;
-	function _then(fn, data) {
+	function _then(fn, data, broadcast) {
 		if (angular.isFunction(fn))
 			fn(data);
-		$rootScope.$broadcast('event:orderUpdate', data);
+		if (!broadcast)
+			$rootScope.$broadcast('event:orderUpdate', data);
 	}
 
 	function _extend(order) {
@@ -33,15 +34,15 @@ four51.app.factory('Order', ['$resource', '$rootScope', '$451', 'Security', 'Err
 		}
 	}
 
-	var _get = function(id, success) {
+	var _get = function(id, success, suppress) {
 		var currentOrder = store.get('451Cache.Order.' + id);
 		currentOrder ? (function() { _extend(currentOrder);	_then(success, currentOrder); })() :
 			$resource($451.api('order/:id'), { id: '@id' }).get({ id: id }).$promise.then(function(o) {
 				_extend(o);
 				store.set('451Cache.Order.' + id, o);
-				_then(success, o);
+				_then(success, o, suppress);
 			});
-	}
+	};
 
 	var _save = function(order, success, error) {
 		$resource($451.api('order')).save(order).$promise.then(
@@ -55,7 +56,7 @@ four51.app.factory('Order', ['$resource', '$rootScope', '$451', 'Security', 'Err
 				error(Error.format(ex));
 			}
 		);
-	}
+	};
 
 	var _delete = function(order, success, error) {
 		$resource($451.api('order')).delete().$promise.then(
@@ -68,7 +69,7 @@ four51.app.factory('Order', ['$resource', '$rootScope', '$451', 'Security', 'Err
 				error(Error.format(ex));
 			}
 		);
-	}
+	};
 
 	var _submit = function(order, success, error) {
 		$resource($451.api('order'), { }, { submit: { method: 'PUT' }}).submit(order).$promise.then(
