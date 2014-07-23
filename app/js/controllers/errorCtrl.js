f38d28eaf821fc53ebb69fd6f4f75bbb4505f091
Steve Davis <sdavis@four51.com>
2014-07-23 11:13:04 -0500

    SPA-12410  Only include trackJS when source is minified

--- app/js/controllers/errorCtrl.js
+++ app/js/controllers/errorCtrl.js
@@ -20,8 +20,6 @@ four51.app.controller('ErrorCtrl', ['$scope', function ($scope) {
 				StackTrace: ex.StackTrace || ''
 			};
 			console.dir($scope.error);
-			//if ($451.debug)
-			//	$scope.open();
 		}
 		catch(e) {
 			console.log('An error occurred while handling an error. Consult the object written to the log. Keep in mind you must use the Error() if you are throwing an error in your JavaScript');
