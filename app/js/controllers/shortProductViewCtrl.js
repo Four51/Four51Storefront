579b5cb328cfe9e6f15717eb684ed80b7f6fadb4
Dan Toivonen <dtoivonen@four51.com>
2014-09-10 17:02:13 -0500

    Undoing Add To Order :(

--- app/js/controllers/shortProductViewCtrl.js
+++ app/js/controllers/shortProductViewCtrl.js
@@ -1,44 +1,6 @@
-four51.app.controller('shortProductViewCtrl', ['$routeParams', '$scope', 'ProductDisplayService', 'Order', 'User', '$location', '$route', function ($routeParams, $scope, ProductDisplayService, Order, User, $location, $route) {
+four51.app.controller('shortProductViewCtrl', ['$routeParams', '$scope', 'ProductDisplayService', 'Order', 'User', '$location', '$route', function ($routeParams, $scope, ProductDisplayService) {
 	$scope.LineItem = {};
 	$scope.LineItem.Product = $scope.p;
 	ProductDisplayService.setNewLineItemScope($scope);
 	ProductDisplayService.setProductViewScope($scope);
-	$scope.allowAddToOrderInProductList = $scope.allowAddToOrder && $scope.LineItem.Product.Type != 'VariableText' && $scope.LineItem.Product.SpecCount == 0;
-    //Product List Add to Order Functionality
-    $scope.addToOrder = function(){
-        $scope.displayLoadingIndicator = true;
-        $scope.actionMessage = null;
-        $scope.errorMessage = null;
-        $scope.user.CurrentOrderID ? addLineItemToCurrentOrder() : addLineItemToNewOrder();
-        $scope.displayLoadingIndicator = false;
-    };
-    var addLineItemToCurrentOrder = function(){
-        Order.get($scope.user.CurrentOrderID, function(order){
-            addToOrderSave(order);
-        });
-    };
-    var addLineItemToNewOrder = function(){
-        var currentOrder = {};
-        currentOrder.LineItems = [];
-        addToOrderSave(currentOrder);
-    };
-    var addToOrderSave = function(currentOrder){
-        currentOrder.LineItems.push($scope.LineItem);
-        Order.save(currentOrder,
-            function(order, callback){
-                $scope.user.CurrentOrderID = order.ID;
-                $scope.LineItem.Product.QuantityAvailable = $scope.LineItem.Product.QuantityAvailable - $scope.LineItem.Quantity;
-                User.save($scope.user, function(){
-                    $scope.LineItem.Quantity = null;
-                });
-                if (callback) callback();
-                $scope.actionMessage = 'Item has been added to your cart!';
-            },
-            function (ex) {
-                $scope.displayLoadingIndicator = false;
-                $scope.errorMessage = ex.Message;
-            }
-        );
-
-    };
 }]);
