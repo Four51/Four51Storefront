f1b4fb1173aa49efc393235ef856585e4d64bafc
Kyle Olson <kolson@four51.com>
2015-01-12 10:53:53 -0600

    SPA-14048 Admin/User - If you navigate directly to the user information page when accessing a site, recieve breaking error

--- app/js/controllers/userEditCtrl.js
+++ app/js/controllers/userEditCtrl.js
@@ -1,67 +1,70 @@
 four51.app.controller('UserEditCtrl', ['$scope', '$location', '$sce', 'User',
 function ($scope, $location, $sce, User) {
-	$scope.loginasuser = {};
-	$scope.actionMessage = null;
-	$scope.securityWarning = false;
+	User.get(function(user) {
+        $scope.user = user;
+        $scope.loginasuser = {};
+        $scope.actionMessage = null;
+        $scope.securityWarning = false;
 
-	if($scope.user.Type != 'TempCustomer')
-		$scope.user.TempUsername = $scope.user.Username
-	$scope.getToken = function(){
-		$scope.loginasuser.SendVerificationCodeByEmail = true;
-		$scope.emailResetLoadingIndicator = true;
-		User.login($scope.loginasuser, function(){
-				$scope.resetPasswordError = null;
-				$scope.enterResetToken = true;
-				$scope.emailResetLoadingIndicator = false;
-			},
-			function(err){
-				$scope.resetPasswordError =  $sce.trustAsHtml(err.Message);
-				$scope.emailResetLoadingIndicator = false;
-			});
+        if ($scope.user.Type != 'TempCustomer')
+            $scope.user.TempUsername = $scope.user.Username
+        $scope.getToken = function () {
+            $scope.loginasuser.SendVerificationCodeByEmail = true;
+            $scope.emailResetLoadingIndicator = true;
+            User.login($scope.loginasuser, function () {
+                    $scope.resetPasswordError = null;
+                    $scope.enterResetToken = true;
+                    $scope.emailResetLoadingIndicator = false;
+                },
+                function (err) {
+                    $scope.resetPasswordError = $sce.trustAsHtml(err.Message);
+                    $scope.emailResetLoadingIndicator = false;
+                });
 
-	}
-	$scope.resetWithToken= function(){
-		$scope.emailResetLoadingIndicator = true;
-		User.reset($scope.loginasuser, function(user){
-				delete $scope.loginasuser;
-				$location.path('catalog');
-			},
-			function(err){
-				$scope.emailResetLoadingIndicator = false;
-				$scope.resetPasswordError = $sce.trustAsHtml(err.Message);
-		});
-	}
-	$scope.save = function() {
-		$scope.actionMessage = null;
-		$scope.securityWarning = false;
-		$scope.user.Username = $scope.user.TempUsername;
-		$scope.displayLoadingIndicator = true;
-        if($scope.user.Type == 'TempCustomer')
-			$scope.user.ConvertFromTempUser = true;
+        }
+        $scope.resetWithToken = function () {
+            $scope.emailResetLoadingIndicator = true;
+            User.reset($scope.loginasuser, function (user) {
+                    delete $scope.loginasuser;
+                    $location.path('catalog');
+                },
+                function (err) {
+                    $scope.emailResetLoadingIndicator = false;
+                    $scope.resetPasswordError = $sce.trustAsHtml(err.Message);
+                });
+        }
+        $scope.save = function () {
+            $scope.actionMessage = null;
+            $scope.securityWarning = false;
+            $scope.user.Username = $scope.user.TempUsername;
+            $scope.displayLoadingIndicator = true;
+            if ($scope.user.Type == 'TempCustomer')
+                $scope.user.ConvertFromTempUser = true;
 
-		User.save($scope.user,
-			function(u) {
-				$scope.securityWarning = false;
-				$scope.displayLoadingIndicator = false;
-				$scope.actionMessage = 'Your changes have been saved';
-				$scope.user.TempUsername = u.Username;
-			},
-			function(ex) {
-				$scope.displayLoadingIndicator = false;
-				if (ex.Code.is('PasswordSecurity'))
-					$scope.securityWarning = true;
-				else {
-					$scope.actionMessage = $sce.trustAsHtml(ex.Message);
-				}
-			}
-		);
-    };
-	$scope.loginExisting = function(){
-		User.login({Username: $scope.loginasuser.Username, Password:  $scope.loginasuser.Password, ID: $scope.user.ID, Type: $scope.user.Type},function(u){
-			$location.path("/catalog");
+            User.save($scope.user,
+                function (u) {
+                    $scope.securityWarning = false;
+                    $scope.displayLoadingIndicator = false;
+                    $scope.actionMessage = 'Your changes have been saved';
+                    $scope.user.TempUsername = u.Username;
+                },
+                function (ex) {
+                    $scope.displayLoadingIndicator = false;
+                    if (ex.Code.is('PasswordSecurity'))
+                        $scope.securityWarning = true;
+                    else {
+                        $scope.actionMessage = $sce.trustAsHtml(ex.Message);
+                    }
+                }
+            );
+        };
+        $scope.loginExisting = function () {
+            User.login({Username: $scope.loginasuser.Username, Password: $scope.loginasuser.Password, ID: $scope.user.ID, Type: $scope.user.Type}, function (u) {
+                $location.path("/catalog");
 
-		}, function(err){
-			$scope.loginAsExistingError = err.Message;
-		});
-	};
+            }, function (err) {
+                $scope.loginAsExistingError = err.Message;
+            });
+        };
+    });
 }]);
