0d36dcc2d7b9317b1b8a79511eb34708f0ac2275
Steve Davis <sdavis@four51.com>
2015-03-25 14:11:47 -0500

    DEV-15304  Product Detail Spec Form - The form doesn't get called again after its been loaded the first time

--- app/js/directives/addToOrderSpecs.js
+++ app/js/directives/addToOrderSpecs.js
@@ -1,8 +1,11 @@
 four51.app.directive('addtoorderspecs', ['$routeParams',  function($routeParams) {
 	var obj = {
-		restrict: 'E',
-		templateUrl: 'addToOrderSpecForm.hcf?id=' + $routeParams.productInteropID
-	};
+        restrict: 'E',
+        template: '<div ng-include="specForm">',
+        link: function (scope) {
+            scope.specForm = 'addToOrderSpecForm.hcf?id=' + $routeParams.productInteropID + '&r=' + Math.random();
+        }
+    };
 	return obj;
 }]);
 
