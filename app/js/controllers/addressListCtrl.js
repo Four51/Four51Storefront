2e3d865e8424fe2b3bc3fccb7b1603dfa6d1ccfc
Kyle Olson <kolson@four51.com>
2015-03-27 16:28:04 -0500

    DEV-15555 Display Admin Assigned Addresses to User

--- app/js/controllers/addressListCtrl.js
+++ app/js/controllers/addressListCtrl.js
@@ -13,7 +13,7 @@ function ($scope, $location, $451, AddressList) {
 			$scope.addresses = list;
 			$scope.settings.listCount = count;
 			$scope.pagedIndicator = false;
-		}, $scope.settings.currentPage, $scope.settings.pageSize, $scope.searchTerm, true);
+		}, $scope.settings.currentPage, $scope.settings.pageSize, $scope.searchTerm, false);
 	}
 
     $scope.deleteSelected = function() {
@@ -33,7 +33,7 @@ function ($scope, $location, $451, AddressList) {
     };
     $scope.checkAll = function(event) {
         angular.forEach($scope.addresses, function(add) {
-            add.Selected = event.currentTarget.checked;
+            if (add.IsCustEditable) add.Selected = event.currentTarget.checked;
         });
     };
 
