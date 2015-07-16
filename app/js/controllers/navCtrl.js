85f9e641fb2916e7d437a44b7429f86629c112fa
FOUR51\tliberty <Think_big_1234>
2015-07-16 09:29:35 -0500

    Fixing Bugs... SPA-14657

--- app/js/controllers/navCtrl.js
+++ app/js/controllers/navCtrl.js
@@ -50,6 +50,6 @@ four51.app.controller('NavCtrl', ['$location', '$route', '$scope', '$451', 'User
         }
 
         $scope.$on('event:orderUpdate', function(event, order) {
-            $scope.cartCount = order ? (order.Status == 'Unsubmitted' || order.Status == 'AwaitingApproval') ? order.LineItems.length : null : null;
+            $scope.cartCount = (order ? ((order.Status == 'Unsubmitted') ? order.LineItems.length : null) : null);
         });
     }]);
