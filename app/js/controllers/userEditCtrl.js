41a9015ccab05d00e95191d67768e2c95d5d0bda
mmoebeck <mmoebeck@four51.com>
2018-06-11 09:38:15 -0500

    COMTOOLS-892 and COMTOOLS-893

--- app/js/controllers/userEditCtrl.js
+++ app/js/controllers/userEditCtrl.js
@@ -18,60 +18,91 @@ four51.app.controller('UserEditCtrl', ['$scope', '$location', '$sce', '$injector
             $scope.getToken = function () {
                 $scope.loginasuser.SendVerificationCodeByEmail = true;
                 $scope.emailResetLoadingIndicator = true;
+                $scope.buttonClicked = true;
                 User.login($scope.loginasuser, function () {
                         $scope.resetPasswordError = null;
                         $scope.enterResetToken = true;
                         $scope.emailResetLoadingIndicator = false;
+                        $scope.buttonClicked = false;
                     },
                     function (err) {
                         $scope.resetPasswordError = $sce.trustAsHtml(err.Message);
                         $scope.emailResetLoadingIndicator = false;
+                        $scope.buttonClicked = false;
                     });
 
             }
             $scope.resetWithToken = function () {
+                $scope.buttonClicked = true;
                 $scope.emailResetLoadingIndicator = true;
                 if($scope.currentOrder){
                     $scope.loginasuser.CurrentOrderID = $scope.currentOrder.ID;
                 }
                 User.reset($scope.loginasuser, function (user) {
-                    delete $scope.loginasuser;
-                    if(user.CurrentOrderID){
-                        Order.get(user.CurrentOrderID,function(order){
-                            $scope.currentOrder = order;
-                            $scope.currentOrder.FromUserID = user.ID;
-                            Order.save($scope.currentOrder,function(ordr){
-                                $location.path('checkout');
+                        delete $scope.loginasuser;
+                        if(user.CurrentOrderID){
+                            Order.get(user.CurrentOrderID,function(order){
+                                $scope.currentOrder = order;
+                                $scope.currentOrder.FromUserID = user.ID;
+                                Order.save($scope.currentOrder,function(ordr){
+                                    $scope.buttonClicked = false;
+                                    $location.path('checkout');
+                                });
                             });
-                        });
-                    }
-                    else{
-                        $location.path('catalog');
-                    }
-                },
-                function (err) {
-                    $scope.emailResetLoadingIndicator = false;
-                    $scope.resetPasswordError = $sce.trustAsHtml(err.Message);
-                });
+                        }
+                        else{
+                            $scope.buttonClicked = false;
+                            $location.path('catalog');
+                        }
+                    },
+                    function (err) {
+                        $scope.buttonClicked = false;
+                        $scope.emailResetLoadingIndicator = false;
+                        $scope.resetPasswordError = $sce.trustAsHtml(err.Message);
+                    });
             }
             $scope.save = function () {
+                $scope.buttonClicked = true;
                 $scope.actionMessage = null;
                 $scope.securityWarning = false;
                 $scope.user.Username = $scope.user.TempUsername;
                 $scope.displayLoadingIndicator = true;
-                if ($scope.user.Type == 'TempCustomer')
+                if ($scope.user.Type == 'TempCustomer'){
                     $scope.user.ConvertFromTempUser = true;
+                }
+                if($scope.currentOrder){
+                    $scope.pendingOrder = angular.copy($scope.currentOrder.ID);
+                }
 
                 User.save($scope.user,
                     function (u) {
-                        $scope.securityWarning = false;
-                        $scope.displayLoadingIndicator = false;
-                        $scope.actionMessage = 'Your changes have been saved';
-                        $scope.user.TempUsername = u.Username;
-                        if (_AnonRouter && !$scope.existingUser) _AnonRouter.route();
+                        $scope.user = u;
+                        if($scope.pendingOrder){
+                            Order.get($scope.pendingOrder,function(order){
+                                $scope.currentOrder = order;
+                                $scope.currentOrder.FromUserID = $scope.user.ID;
+                                Order.save($scope.currentOrder,function(ordr){
+                                    $scope.securityWarning = false;
+                                    $scope.displayLoadingIndicator = false;
+                                    $scope.buttonClicked = false;
+                                    $scope.actionMessage = 'Your changes have been saved';
+                                    $scope.user.TempUsername = u.Username;
+                                    if (_AnonRouter && !$scope.existingUser) _AnonRouter.route();
+                                });
+                            });
+                        }
+                        else{
+                            $scope.securityWarning = false;
+                            $scope.displayLoadingIndicator = false;
+                            $scope.buttonClicked = false;
+                            $scope.actionMessage = 'Your changes have been saved';
+                            $scope.user.TempUsername = u.Username;
+                            if (_AnonRouter && !$scope.existingUser) _AnonRouter.route();
+                        }
                     },
                     function (ex) {
                         $scope.displayLoadingIndicator = false;
+                        $scope.buttonClicked = false;
                         if (ex.Code.is('PasswordSecurity'))
                             $scope.securityWarning = true;
                         else {
@@ -89,9 +120,29 @@ four51.app.controller('UserEditCtrl', ['$scope', '$location', '$sce', '$injector
                 );
             };
             $scope.loginExisting = function () {
+                $scope.buttonClicked = true;
+                if($scope.currentOrder){
+                    $scope.pendingOrder = angular.copy($scope.currentOrder.ID);
+                }
                 User.login({Username: $scope.loginasuser.Username, Password: $scope.loginasuser.Password, ID: $scope.user.ID, Type: $scope.user.Type}, function (u) {
-                    if (_AnonRouter) _AnonRouter.route();
+                    $scope.user = u;
+                    if($scope.pendingOrder){
+                        Order.get($scope.pendingOrder,function(order){
+                            $scope.currentOrder = order;
+                            $scope.currentOrder.FromUserID = $scope.user.ID;
+                            Order.save($scope.currentOrder,function(ordr){
+                                $scope.buttonClicked = false;
+                                if (_AnonRouter) _AnonRouter.route();
+                            });
+                        });
+                    }
+                    else{
+                        $scope.buttonClicked = false;
+                        if (_AnonRouter) _AnonRouter.route();
+                    }
+
                 }, function (err) {
+                    $scope.buttonClicked = false;
                     $scope.loginAsExistingError = err.Message;
                 });
             };
