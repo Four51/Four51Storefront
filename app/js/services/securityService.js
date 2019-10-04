a0661f0bd7f2590e019f148151a46c9c1c45da7e
mmoebeck <mmoebeck@four51.com>
2019-10-04 14:39:07 -0500

    COMTOOLS-1266 - 2.0 User Cookie - Various pieces of user data that is not encrypted

--- app/js/services/securityService.js
+++ app/js/services/securityService.js
@@ -3,28 +3,25 @@ four51.app.factory('Security', ['$451', '$cookieStore', function($451, $cookieSt
     var logout = false;
     return {
         init: function(user, auth) {
-            this.currentUser = {
-                SiteID: user.SiteID,
-                Username: user.Username,
-                InteropID: user.InteropID,
-                FirstName: user.FirstName,
-                LastName: user.LastName,
-                Email: user.Email,
-                Auth: auth
-            };
+            this.currentToken = auth;
             logout = false;
-            $cookieStore.put(_cookieName, this.currentUser);
+            $cookieStore.put(_cookieName, this.currentToken);
         },
         clear: function() {
             $cookieStore.remove(_cookieName);
         },
         auth: function() {
-            var user = $cookieStore.get(_cookieName);
-            return user ? user.Auth : null;
+            var token = $cookieStore.get(_cookieName);
+            if(token && token.Auth){
+                var tokenString = angular.copy(token.Auth);
+                token = null;
+                token = tokenString;
+            }
+            return token ? token : null;
         },
         isAuthenticated: function() {
-            if (!logout) this.currentUser = $cookieStore.get(_cookieName);
-            return (!!this.currentUser);
+            if (!logout) this.currentToken = $cookieStore.get(_cookieName);
+            return (!!this.currentToken);
         },
         logout: function() {
             logout = true;
@@ -33,7 +30,7 @@ four51.app.factory('Security', ['$451', '$cookieStore', function($451, $cookieSt
                 document.cookie = name + '=; path=/' + $451.apiName + '/; expires=Thu, 01 Jan 1970 00:00:00 UTC';
             }
             delete_cookie(_cookieName);
-            delete this.currentUser;
+            delete this.currentToken;
         }
     }
 }]);
