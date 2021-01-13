8fa116f9289241db37886875851da6cf60afa5f8
Mike Moebeck <mmoebeck@four51.com>
2021-01-13 13:53:40 -0600

    COMTOOLS-1947 - Storefront 2.0 - Add logic to disable access token after logout and session expire after 15 minutes of inactivity

--- app/js/controllers/navCtrl.js
+++ app/js/controllers/navCtrl.js
@@ -1,51 +1,54 @@
-four51.app.controller('NavCtrl', ['$location', '$route', '$scope', '$451', 'User',
-    function ($location, $route, $scope, $451, User) {
-        $scope.Logout = function(){
-            User.logout();
+four51.app.controller('NavCtrl', ['$location', '$route', '$scope', '$451', '$timeout', 'User',
+function ($location, $route, $scope, $451, $timeout, User) {
+    $scope.Logout = function(){
+        User.logout($scope.user, function(u){
             if ($scope.isAnon) {
-                $location.path("/catalog");
-                User.login(function(user) {
-                    $scope.user = user;
-                });
+                $timeout(function () {
+                    $location.path("/catalog");
+                    location.reload(true);
+                }, 500);
             }
-        };
-
-        // http://stackoverflow.com/questions/12592472/how-to-highlight-a-current-menu-item-in-angularjs
-        $scope.isActive = function(path) {
-            var cur_path = $location.path().replace('/', '');
-            var result = false;
-
-            if (path instanceof Array) {
-                angular.forEach(path, function(p) {
-                    if (p == cur_path && !result)
-                        result = true;
-                });
-            }
-            else {
-                if (cur_path == path)
-                    result = true;
-            }
-            return result;
-        };
-        // extension of above isActive in path
-        $scope.isInPath = function(path) {
-            var cur_path = $location.path().replace('/', '');
-            var result = false;
-
-            if(cur_path.indexOf(path) > -1) {
-                result = true;
-            }
-            else {
-                result = false;
-            }
-            return result;
-        };
-
-        $scope.Clear = function() {
-            localStorage.clear();
-        }
-
-        $scope.$on('event:orderUpdate', function(event, order) {
-            $scope.cartCount = (order ? ((order.Status == 'Unsubmitted') ? order.LineItems.length : null) : null);
+        }, function(ex){
+            console.log(ex.Message);
         });
-    }]);
+    };
+
+    // http://stackoverflow.com/questions/12592472/how-to-highlight-a-current-menu-item-in-angularjs
+    $scope.isActive = function(path) {
+        var cur_path = $location.path().replace('/', '');
+        var result = false;
+
+        if (path instanceof Array) {
+            angular.forEach(path, function(p) {
+                if (p == cur_path && !result)
+                    result = true;
+            });
+        }
+        else {
+            if (cur_path == path)
+                result = true;
+        }
+        return result;
+    };
+    // extension of above isActive in path
+    $scope.isInPath = function(path) {
+        var cur_path = $location.path().replace('/', '');
+        var result = false;
+
+        if(cur_path.indexOf(path) > -1) {
+            result = true;
+        }
+        else {
+            result = false;
+        }
+        return result;
+    };
+
+    $scope.Clear = function() {
+        localStorage.clear();
+    }
+
+    $scope.$on('event:orderUpdate', function(event, order) {
+        $scope.cartCount = (order ? ((order.Status == 'Unsubmitted') ? order.LineItems.length : null) : null);
+    });
+}]);
