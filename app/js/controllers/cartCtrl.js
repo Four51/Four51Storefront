35185dd786f258496347ecc5c4b1c6725f2f4699
Rick Forshee <rick.forshee@vividimpact.com>
2023-01-26 10:59:05 -0500

    fix gl codes not applying to cost center

--- app/js/controllers/cartCtrl.js
+++ app/js/controllers/cartCtrl.js
@@ -1,253 +1,247 @@
-var angScope = "";
+var angScope;
+
 four51.app.controller('CartViewCtrl', ['$scope', '$routeParams', '$location', '$451', 'Order', 'OrderConfig', 'User',
-function ($scope, $routeParams, $location, $451, Order, OrderConfig, User) {
-	$scope.isEditforApproval = $routeParams.id != null && $scope.user.Permissions.contains('EditApprovalOrder');
-	if ($scope.isEditforApproval) {
-		Order.get($routeParams.id, function(order) {
-			$scope.currentOrder = order;
-			// add cost center if it doesn't exists for the approving user
-			var exists = false;
-			angular.forEach(order.LineItems, function(li) {
-				angular.forEach($scope.user.CostCenters, function(cc) {
-					if (exists) return;
-					exists = cc == li.CostCenter;
+	function ($scope, $routeParams, $location, $451, Order, OrderConfig, User) {
+		$scope.submitted = false;
+		$scope.isEditforApproval = $routeParams.id != null && $scope.user.Permissions.contains('EditApprovalOrder');
+		if ($scope.isEditforApproval) {
+			Order.get($routeParams.id, function(order) {
+				$scope.currentOrder = order;
+				// add cost center if it doesn't exists for the approving user
+				var exists = false;
+				angular.forEach(order.LineItems, function(li) {
+					angular.forEach($scope.user.CostCenters, function(cc) {
+						if (exists) return;
+						exists = cc == li.CostCenter;
+					});
+					if (!exists) {
+						$scope.user.CostCenters.push({
+							'Name': li.CostCenter
+						});
+					}
 				});
-				if (!exists) {
-					$scope.user.CostCenters.push({
-						'Name': li.CostCenter
-					});
-				}
 			});
-		});
-	}
-
-	$scope.checkCostCenters = function() {
-		for (var i in $scope.currentOrder.LineItems) {
-			var item = $scope.currentOrder.LineItems[i];
-			if (!item.facilityNumber || !item.dept || !item.glCode || (item.glCode == 'Other' && !item.otherText)) {
-				alert('Facility, department, and GL Code information is required for all items');
-				return false;
-			}
-			if (item.glCode == 'Other') {
-				$scope.currentOrder.LineItems[i].CostCenter = item.facilityNumber + '_' + item.dept + '_OTHER:' + item.otherText;
-			} else {
-				$scope.currentOrder.LineItems[i].CostCenter = item.facilityNumber + '_' + item.dept + '_' + item.glCode;
-			}
 		}
-		return true;
-	}
-
-	$scope.getDateNeeded = function() {
-		var days = 5; // Number of business days to add to today
-		var today = new Date();
-		var dayOfWeek = today.getDay();
-		var daysToAdd = parseInt(days);
-		if (dayOfWeek === 0) {
-			daysToAdd++;
-		}
-		if (dayOfWeek + daysToAdd >= 6) {
-			var remainingWorkDays = daysToAdd - (5 - dayOfWeek);
-			daysToAdd += 2;
-			if (remainingWorkDays > 5) {
-				daysToAdd += 2 * Math.floor(remainingWorkDays / 5);
-				if (remainingWorkDays % 5 === 0) {
-					daysToAdd -= 2;
+		$scope.checkCostCenters = function() {
+			for (var i in $scope.currentOrder.LineItems) {
+				var item = $scope.currentOrder.LineItems[i];
+				if (!item.facilityNumber || !item.dept || !item.glCode || (item.glCode == 'Other' && !item.otherText)) {
+					alert('Facility, department, and GL Code information is required for all items');
+					return false;
+				}
+				if (item.glCode == 'Other') {
+					$scope.currentOrder.LineItems[i].CostCenter = item.facilityNumber + '_' + item.dept + '_OTHER:' + item.otherText;
+				} else {
+					$scope.currentOrder.LineItems[i].CostCenter = item.facilityNumber + '_' + item.dept + '_' + item.glCode;
 				}
 			}
+			return true;
 		}
-		today.setDate(today.getDate() + daysToAdd);
-		return today;
-	}
-
-	var dateNeeded = $scope.getDateNeeded();
-	$scope.shipDate =  (dateNeeded.getMonth() + 1) + '/' + dateNeeded.getDate() + '/' + dateNeeded.getFullYear();
-
-	$scope.errorMessage = null;
-	$scope.continueShopping = function() {
-		if (!$scope.cart.$invalid) {
-			if (confirm('Do you want to save changes to your order before continuing?') == true)
-				$scope.saveChanges(function() { $location.path('catalog') });
-		}
-		else
-			$location.path('catalog');
-	};
-
-	$scope.cancelOrder = function() {
-		if (confirm('Are you sure you wish to cancel your order?') == true) {
-			$scope.displayLoadingIndicator = true;
-			$scope.actionMessage = null;
-			Order.delete($scope.currentOrder,
-				function(){
-					$scope.currentOrder = null;
-					$scope.user.CurrentOrderID = null;
-					User.save($scope.user, function(){
-						$location.path('catalog');
-					});
-					$scope.displayLoadingIndicator = false;
-					$scope.actionMessage = 'Your Changes Have Been Saved';
-				},
-				function(ex) {
-					$scope.actionMessage = 'An error occurred: ' + ex.Message;
-					$scope.displayLoadingIndicator = false;
-				}
-			);
-		}
-	};
-
-	$scope.saveChanges = function(callback) {
-		$scope.actionMessage = null;
+		$scope.currentDate = new Date();
 		$scope.errorMessage = null;
-		if($scope.currentOrder.LineItems.length == $451.filter($scope.currentOrder.LineItems, {Property:'Selected', Value: true}).length) {
-			$scope.cancelOrder();
+		$scope.continueShopping = function() {
+			if (!$scope.cart.$invalid) {
+				if (confirm('Do you want to save changes to your order before continuing?') == true)
+					$scope.saveChanges(function() { $location.path('home') });
+			}
+			else
+				$location.path('home');
+		};
+
+		$scope.cancelOrder = function() {
+			if (confirm('Are you sure you wish to cancel your order?') == true) {
+				$scope.displayLoadingIndicator = true;
+				$scope.actionMessage = null;
+				Order.delete($scope.currentOrder,
+					function(){
+						$scope.currentOrder = null;
+						$scope.user.CurrentOrderID = null;
+						User.save($scope.user, function(){
+							$location.path('home');
+						});
+						$scope.displayLoadingIndicator = false;
+						$scope.actionMessage = 'Your Changes Have Been Saved';
+					},
+					function(ex) {
+						$scope.actionMessage = 'An error occurred: ' + ex.Message;
+						$scope.displayLoadingIndicator = false;
+					}
+				);
+			}
+		};
+
+		$scope.saveChanges = function(callback) {
+			if (!$scope.checkCostCenters()) {
+				return;
+			}
+			$scope.actionMessage = null;
+			$scope.errorMessage = null;
+			if ($scope.currentOrder.LineItems.length == $451.filter($scope.currentOrder.LineItems, {Property:'Selected', Value: true}).length) {
+				$scope.cancelOrder();
+			}
+			else {
+				$scope.displayLoadingIndicator = true;
+				OrderConfig.address($scope.currentOrder, $scope.user);
+				Order.save($scope.currentOrder,
+					function(data) {
+						$scope.displayLoadingIndicator = false;
+						if (callback) callback();
+						$scope.actionMessage = 'Your Changes Have Been Saved';
+					},
+					function(ex) {
+						$scope.errorMessage = ex.Message;
+						$scope.displayLoadingIndicator = false;
+					}
+				);
+			}
+		};
+
+		$scope.removeItem = function(item) {
+			if (confirm('Are you sure you wish to remove this item from your cart?') == true) {
+				Order.deletelineitem($scope.currentOrder.ID, item.ID,
+					function(order) {
+						$scope.currentOrder = order;
+						Order.clearshipping($scope.currentOrder);
+						if (!order) {
+							$scope.user.CurrentOrderID = null;
+							User.save($scope.user, function(){
+								$location.path('home');
+							});
+						}
+						$scope.displayLoadingIndicator = false;
+						$scope.actionMessage = 'Your Changes Have Been Saved';
+					},
+					function (ex) {
+						$scope.errorMessage = ex.Message.replace(/\<<Approval Page>>/g, 'Approval Page');
+						$scope.displayLoadingIndicator = false;
+					}
+				);
+			}
 		}
-		else {
+
+		$scope.checkOut = function() {
+			$scope.submitted = true;
+			if (!$scope.checkCostCenters()) {
+				return;
+			}
 			$scope.displayLoadingIndicator = true;
-			OrderConfig.address($scope.currentOrder, $scope.user);
-			Order.save($scope.currentOrder,
-				function(data) {
+			if (!$scope.isEditforApproval) {
+				OrderConfig.address($scope.currentOrder, $scope.user);
+			}
+			for (var i in $scope.currentOrder.LineItems) {
+				$scope.currentOrder.LineItems[i].DateNeeded = $scope.getDateNeeded();
+			}
+			Order.save($scope.currentOrder, function(data) {
 					$scope.currentOrder = data;
+					$location.path($scope.isEditforApproval ? 'checkout/' + $routeParams.id : 'checkout');
 					$scope.displayLoadingIndicator = false;
-					if (callback) callback();
-					$scope.actionMessage = 'Your Changes Have Been Saved';
 				},
 				function(ex) {
 					$scope.errorMessage = ex.Message;
 					$scope.displayLoadingIndicator = false;
 				}
 			);
-		}
-	};
+		};
 
-	$scope.removeItem = function(item) {
-		if (confirm('Are you sure you wish to remove this item from your cart?') == true) {
-			Order.deletelineitem($scope.currentOrder.ID, item.ID,
-				function(order) {
-					$scope.currentOrder = order;
-					Order.clearshipping($scope.currentOrder);
-					if (!order) {
-						$scope.user.CurrentOrderID = null;
-						User.save($scope.user, function(){
-							$location.path('catalog');
-						});
-					}
-					$scope.displayLoadingIndicator = false;
-					$scope.actionMessage = 'Your Changes Have Been Saved';
-				},
-				function (ex) {
-					$scope.errorMessage = ex.Message.replace(/\<<Approval Page>>/g, 'Approval Page');
-					$scope.displayLoadingIndicator = false;
+		$scope.$watch('currentOrder.LineItems', function(newval) {
+			var newTotal = 0;
+			if (!$scope.currentOrder) {
+				return newTotal;
+			}
+			angular.forEach($scope.currentOrder.LineItems, function(item){
+				if (!item.Product.SmallImageUrl){
+					angular.forEach($scope.currentOrder.LineItems,function(li){
+						if (item.Product.InteropID == li.Product.InteropID && (li.Product.SmallImageUrl)){
+							item.Product.SmallImageUrl = li.Product.SmallImageUrl;
+						}
+					});
 				}
-			);
-		}
-	}
+				if (item.IsKitParent) {
+					$scope.cart.$setValidity('kitValidation', !item.KitIsInvalid);
+				}
+				newTotal += item.LineTotal;
+			});
+			$scope.currentOrder.Subtotal = newTotal;
+		}, true);
 
-	$scope.checkOut = function() {
-		$scope.displayLoadingIndicator = true;
-		if (!$scope.isEditforApproval)
-			OrderConfig.address($scope.currentOrder, $scope.user);
-		Order.save($scope.currentOrder,
-			function(data) {
-				$scope.currentOrder = data;
-                $location.path($scope.isEditforApproval ? 'checkout/' + $routeParams.id : 'checkout');
-				$scope.displayLoadingIndicator = false;
-			},
-			function(ex) {
-				$scope.errorMessage = ex.Message;
-				$scope.displayLoadingIndicator = false;
+		$scope.copyAddressToAll = function() {
+			angular.forEach($scope.currentOrder.LineItems, function(n) {
+				n.DateNeeded = $scope.currentOrder.LineItems[0].DateNeeded;
+			});
+		};
+
+		$scope.onPrint = function() {
+			window.print();
+		};
+
+		$scope.cancelEdit = function() {
+			$location.path('order');
+		};
+
+		$scope.downloadProof = function(item) {
+			window.location = item.Variant.ProofUrl;
+		};
+		$scope.getDateNeeded = function() {
+			var days = 5; // Number of business days to add to today
+			var today = new Date();
+			var dayOfWeek = today.getDay();
+			var daysToAdd = parseInt(days);
+			if (dayOfWeek == 0) {
+				daysToAdd++;
 			}
-		);
-	};
-
-	$scope.$watch('currentOrder.LineItems', function(newval) {
-		var newTotal = 0;
-		if (!$scope.currentOrder) return newTotal;
-		angular.forEach($scope.currentOrder.LineItems, function(item){
-			if (item.IsKitParent)
-				$scope.cart.$setValidity('kitValidation', !item.KitIsInvalid);
-			newTotal += item.LineTotal;
-		});
-		$scope.currentOrder.Subtotal = newTotal;
-	}, true);
-
-	$scope.copyAddressToAll = function() {
-		angular.forEach($scope.currentOrder.LineItems, function(n) {
-			n.DateNeeded = $scope.currentOrder.LineItems[0].DateNeeded;
-		});
-	};
-
-	$scope.copyCostCenterToAll = function() {
-		angular.forEach($scope.currentOrder.LineItems, function(n) {
-			n.CostCenter = $scope.currentOrder.LineItems[0].CostCenter;
-		});
-	};
-
-	$scope.copyDepartmentToAll = function() {
-		angular.forEach($scope.currentOrder.LineItems, function(n) {
-			n.Department = $scope.currentOrder.LineItems[0].Department;
-		});
-	};
-
-	$scope.copyFacilityToAll = function() {
-		angular.forEach($scope.currentOrder.LineItems, function(n) {
-			n.Facility = $scope.currentOrder.LineItems[0].Facility;
-		});
-	};
-	
-	$scope.copyGLCodeToAll = function() {
-		angular.forEach($scope.currentOrder.LineItems, function(n) {
-			n.GLCode = $scope.currentOrder.LineItems[0].GLCode;
-		});
-	};	
-
-	$scope.onPrint = function()  {
-		window.print();
-	};
-
-	$scope.cancelEdit = function() {
-		$location.path('order');
-	};
-
-    $scope.downloadProof = function(item) {
-        window.location = item.Variant.ProofUrl;
-    };
-
-	$scope.glCodeChanged = function(item) {
-		if (item.glCode == 'Other') {
-			item.showOther = true;
-		} else {
-			item.showOther = false;
+			if (dayOfWeek + daysToAdd >= 6) {
+				var remainingWorkDays = daysToAdd - (5 - dayOfWeek);
+				daysToAdd += 2;
+				if (remainingWorkDays > 5) {
+					daysToAdd += 2 * Math.floor(remainingWorkDays / 5);
+					if (remainingWorkDays % 5 == 0) {
+						daysToAdd -= 2;
+					}
+				}
+			}
+			today.setDate(today.getDate() + daysToAdd);
+			return today;
 		}
-	}
-	$scope.copyToAll = function(item) {
-		for (var i in $scope.currentOrder.LineItems) {
-			$scope.currentOrder.LineItems[i].facilityNumber = item.facilityNumber;
-			$scope.currentOrder.LineItems[i].dept = item.dept;
-			$scope.currentOrder.LineItems[i].glCode = item.glCode;
+		$scope.glCodeChanged = function(item) {
 			if (item.glCode == 'Other') {
-				$scope.currentOrder.LineItems[i].showOther = true;
-				$scope.currentOrder.LineItems[i].otherText = item.otherText;
+				item.showOther = true;
 			} else {
+				item.showOther = false;
+			}
+		}
+		$scope.copyToAll = function(item) {
+			for (var i in $scope.currentOrder.LineItems) {
+				$scope.currentOrder.LineItems[i].facilityNumber = item.facilityNumber;
+				$scope.currentOrder.LineItems[i].dept = item.dept;
+				$scope.currentOrder.LineItems[i].glCode = item.glCode;
+				if (item.glCode == 'Other') {
+					$scope.currentOrder.LineItems[i].showOther = true;
+					$scope.currentOrder.LineItems[i].otherText = item.otherText;
+				} else {
+					$scope.currentOrder.LineItems[i].showOther = false;
+					$scope.currentOrder.LineItems[i].otherText = null;
+				}
+			}
+		}
+		$scope.$watch('orderInitialized', function() {
+			// Initiate Facility, Dept and GL Code data if it already exists:
+			for (var i in $scope.currentOrder.LineItems) {
 				$scope.currentOrder.LineItems[i].showOther = false;
-				$scope.currentOrder.LineItems[i].otherText = null;
+				if ($scope.currentOrder.LineItems[i].CostCenter !== 'undefined' && $scope.currentOrder.LineItems[i].CostCenter) {
+					var elements = $scope.currentOrder.LineItems[i].CostCenter.split('_');
+					$scope.currentOrder.LineItems[i].showOther = false;
+					$scope.currentOrder.LineItems[i].facilityNumber = elements[0];
+					$scope.currentOrder.LineItems[i].dept = elements[1];
+					$scope.currentOrder.LineItems[i].glCode = elements[2];
+					if ($scope.currentOrder.LineItems[i].CostCenter.indexOf('OTHER:') !== -1) {
+						$scope.currentOrder.LineItems[i].glCode = 'Other';
+						$scope.currentOrder.LineItems[i].showOther = true;
+						$scope.currentOrder.LineItems[i].otherText = $scope.currentOrder.LineItems[i].CostCenter.split('OTHER:')[1];
+					}
+				}
 			}
-		}
-	}
-
-	// Initiate Facility, Dept and GL Code data if it already exists:
-	for (var i in $scope.currentOrder.LineItems) {
-		$scope.currentOrder.LineItems[i].showOther = false;
-		if ($scope.currentOrder.LineItems[i].CostCenter !== 'undefined' && $scope.currentOrder.LineItems[i].CostCenter) {
-			var elements = $scope.currentOrder.LineItems[i].CostCenter.split('_');
-			$scope.currentOrder.LineItems[i].showOther = false;
-			$scope.currentOrder.LineItems[i].facilityNumber = elements[0];
-			$scope.currentOrder.LineItems[i].dept = elements[1];
-			$scope.currentOrder.LineItems[i].glCode = elements[2];
-			if ($scope.currentOrder.LineItems[i].CostCenter.indexOf('OTHER:') !== -1) {
-				$scope.currentOrder.LineItems[i].glCode = 'Other';
-				$scope.currentOrder.LineItems[i].showOther = true;
-				$scope.currentOrder.LineItems[i].otherText = $scope.currentOrder.LineItems[i].CostCenter.split('OTHER:')[1];
-			}
-		}
-	}
-	angScope = $scope;
-}]);
+		});
+		var dateNeeded = $scope.getDateNeeded();
+		$scope.shipDate =  (dateNeeded.getMonth() + 1) + '/' + dateNeeded.getDate() + '/' + dateNeeded.getFullYear();
+		angScope = $scope;
+	}]);
