0fb331f6f49a9f2a1898bd6c9c8b3981c79711c2
Hailey Rissolo <hailey.rissolo@vividimpact.com>
2024-03-19 09:58:54 -0400

    - changed price schedule view & order quantity to make 11 === "Download"

--- app/js/directives/quantityfield.js
+++ app/js/directives/quantityfield.js
@@ -27,11 +27,15 @@ four51.app.directive('quantityfield', ['$451', 'ProductDisplayService', function
             };
             scope.qtyChanged = function(lineitem){
                 // lineitem.Specs.DownloadUrl.Value = '';
-                if (lineitem.Product.Type === 'Static' && lineitem.PriceSchedule.Name === 'Downloadable' && lineitem.Quantity === 11 && lineitem.Product.StaticSpecGroups['Special'].Specs['04 - Proof'].FileURL) {
-                    let downloadUrl = location.origin.replace('teststore', 'test') + '/UI' + lineitem.Product.StaticSpecGroups['Special'].Specs['04 - Proof'].FileURL;
+                if (lineitem.Product.Type === 'Static' && lineitem.PriceSchedule.Name === 'downloadable' && lineitem.Quantity === 11 && lineitem.Product.StaticSpecGroups['Download'].Specs['Paid'].FileURL) {
+                    let downloadUrl = location.origin.replace('teststore', 'test') + '/UI' + lineitem.Product.StaticSpecGroups['Download'].Specs['Paid'].FileURL;
                     lineitem.Specs.DownloadUrl.Value = downloadUrl;
                 }
-                if (lineitem.Product.Type === 'VariableText' && lineitem.PriceSchedule.Name === 'Downloadable' && lineitem.Quantity === 11) {
+                if (lineitem.Product.Type === 'Static' && lineitem.PriceSchedule.Name === 'downloadable' && lineitem.Quantity === 11 && lineitem.Product.StaticSpecGroups['Download'].Specs['Paid'].FileURL === null) {
+                    let downloadUrl = location.origin.replace('teststore', 'test') + '/UI' + lineitem.Product.StaticSpecGroups['Download'].Specs['Paid'].FileURL;
+                    lineitem.Specs.DownloadUrl.Value = downloadUrl;
+                }
+                if (lineitem.Product.Type === 'VariableText' && lineitem.PriceSchedule.Name === 'downloadable' && lineitem.Quantity === 11) {
                     let downloadUrl = lineitem.Variant.ProductionURL;
                     lineitem.Specs.DownloadUrl.Value = downloadUrl;
                 }
